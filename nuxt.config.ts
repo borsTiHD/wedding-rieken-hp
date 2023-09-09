b536bf60eff51edb1987840151069152f4329bf5
borsTiHD <bastian.j@gmx.net>
2023-09-09 18:51:45 +0200

    style: 🔧 Changing env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     runtimeConfig: {
-        firebase_apiKey: '', // can be overridden by NUXT_API_SECRET environment variable
-        firebase_authDomain: '',
-        firebase_projectId: '',
-        firebase_storageBucket: '',
-        firebase_messagingSenderId: '',
-        firebase_appId: '',
-        firebase_measurementId: ''
+        firebaseApikey: '', // can be overridden by NUXT_API_SECRET environment variable
+        firebaseAuthdomain: '',
+        firebaseProjectid: '',
+        firebaseStoragebucket: '',
+        firebaseMessagingsenderid: '',
+        firebaseAppid: '',
+        firebaseMeasurementid: ''
     },
     css: [
         '~/assets/css/tailwindcss.css', // tailwindcss
