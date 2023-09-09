7968eb3bcf6959734951924a0995daa218885f2c
borsTiHD <bastian.j@gmx.net>
2023-09-09 15:57:35 +0200

    feat: 👽️ Adding firebase lib to api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    runtimeConfig: {
+        firebase_apiKey: '', // can be overridden by NUXT_API_SECRET environment variable
+        firebase_authDomain: '',
+        firebase_projectId: '',
+        firebase_storageBucket: '',
+        firebase_messagingSenderId: '',
+        firebase_appId: '',
+        firebase_measurementId: ''
+    },
     devtools: { enabled: true }
 })
