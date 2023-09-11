ceac9099c05c9cb5b6a4c7437917d6a7fd852631
borsTiHD <bastian.j@gmx.net>
2023-09-11 12:10:42 +0200

    feat: ✨ Adding toast service

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="app-wrapper">
+        <Toast position="bottom-right" />
         <LoadingOverlay v-if="loading" :progress="progress" />
         <main v-else>
             <NuxtPage />
--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,9 +1,18 @@
 import { defineNuxtPlugin } from '#app'
 import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
 import Button from 'primevue/button'
+import Calendar from 'primevue/calendar'
+import Toast from 'primevue/toast'
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+
+    // Toast service
+    nuxtApp.vueApp.use(ToastService)
+
+    // PrimeVue components
     nuxtApp.vueApp.component('Button', Button)
-    //other components that you need
+    nuxtApp.vueApp.component('Calendar', Calendar)
+    nuxtApp.vueApp.component('Toast', Toast)
 })
