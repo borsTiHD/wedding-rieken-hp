911184dfffb6f3f269e9b6285325ee0a149b2ac1
borsTiHD <bastian.j@gmx.net>
2023-09-13 19:06:07 +0200

    feat: 🎨 Login form refactored

--- src/components/login/LoginForm.vue
+++ src/components/login/LoginForm.vue
@@ -1,21 +1,27 @@
 <template>
-    <div class="flex gap-4">
+    <form class="flex flex-column gap-2" @submit.prevent="onSubmit">
         <div class="p-inputgroup flex-1">
             <span class="p-inputgroup-addon">
                 <i class="pi pi-user" />
             </span>
-            <InputText v-model="username" placeholder="Username" />
+            <InputText v-model="username" placeholder="Username" name="username" autocomplete="username" />
         </div>
         <div class="p-inputgroup flex-1">
             <span class="p-inputgroup-addon">
                 <i class="pi pi-lock" />
             </span>
-            <Password v-model="password" placeholder="Password" toggleMask />
+            <Password v-model="password" placeholder="Password" name="password" autocomplete="current-password" toggleMask />
         </div>
-    </div>
+
+        <Button type="submit" severity="success" label="Sign In" />
+    </form>
 </template>
 
 <script setup lang="ts">
 const username = ref('')
 const password = ref('')
+
+const onSubmit = () => {
+    console.log(username.value, password.value)
+}
 </script>
