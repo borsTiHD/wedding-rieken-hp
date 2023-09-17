c3a49cc917c5227fd0bc73b69baa8340a0e8dcda
borsTiHD <bastian.j@gmx.net>
2023-09-17 21:40:07 +0200

    refactor: ✨ Simplifing admin check routine

--- src/server/api/users/add.post.ts
+++ src/server/api/users/add.post.ts
@@ -1,43 +1,8 @@
 import { auth, db } from '@/server/lib/firebaseAdmin'
+import checkAdmin from '@/server/lib/checkAdmin'
 
 export default defineEventHandler(async(event) => {
-    const idToken = getCookie(event, 'user-id-token')
-    if (!idToken) {
-        throw createError({
-            statusCode: 401,
-            statusMessage: 'You must be signed in to access to resource.'
-        })
-    }
-
-    // Verify ID token
-    const decodedToken = await auth.verifyIdToken(idToken).catch((error) => {
-        throw createError({
-            statusMessage: error.message
-        })
-    })
-
-    // Check if user exists
-    const user = await auth.getUser(decodedToken.uid).catch((error) => {
-        throw createError({
-            statusMessage: error.message
-        })
-    })
-
-    // Get user profile under /users/{userId}
-    const userDoc = await db.collection('users').doc(user.uid).get().catch((error) => {
-        throw createError({
-            statusMessage: error.message
-        })
-    })
-
-    // Check if user profile exists and if role is admin
-    const userProfile = userDoc.data()
-    if (!userProfile || userProfile.role !== 'admin') {
-        throw createError({
-            statusCode: 403,
-            statusMessage: 'You must be an admin to access to resource.'
-        })
-    }
+    await checkAdmin(event)
 
     // Add new user
     // body: {
---
+++ src/server/lib/checkAdmin.ts
@@ -0,0 +1,44 @@
+import { auth, db } from '@/server/lib/firebaseAdmin'
+import type { H3Event } from 'h3'
+
+export default async function checkAdmin(event: H3Event) {
+    const idToken = getCookie(event, 'user-id-token')
+    if (!idToken) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'You must be signed in to access.'
+        })
+    }
+
+    // Verify ID token
+    const decodedToken = await auth.verifyIdToken(idToken).catch((error) => {
+        throw createError({
+            statusMessage: error.message
+        })
+    })
+
+    // Check if user exists
+    const user = await auth.getUser(decodedToken.uid).catch((error) => {
+        throw createError({
+            statusMessage: error.message
+        })
+    })
+
+    // Get user profile under /users/{userId}
+    const userDoc = await db.collection('users').doc(user.uid).get().catch((error) => {
+        throw createError({
+            statusMessage: error.message
+        })
+    })
+
+    // Check if user profile exists and if role is admin
+    const userProfile = userDoc.data()
+    if (!userProfile || userProfile.role !== 'admin') {
+        throw createError({
+            statusCode: 403,
+            statusMessage: 'You must be an admin to access.'
+        })
+    }
+
+    return true
+}
