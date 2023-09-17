799c0469929cff88e109418a0a2a322575adc658
borsTiHD <bastian.j@gmx.net>
2023-09-17 11:19:30 +0200

    feat: 💄 Formkit styling changed

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,6 +1,6 @@
 import { defineFormKitConfig } from '@formkit/vue'
 import { createAutoAnimatePlugin } from '@formkit/addons'
-// import { generateClasses } from '@formkit/themes'
+import { generateClasses } from '@formkit/themes'
 import { de } from '@formkit/i18n'
 
 // Default Theme
@@ -14,38 +14,10 @@ export default defineFormKitConfig({
         createAutoAnimatePlugin()
     ],
     config: {
-        // classes: generateClasses({
-        //     text: {
-        //         label: 'dark:text-white',
-        //         help: 'dark:text-white',
-        //         input: 'placeholder:text-neutral-500 dark:text-white',
-        //         wrapper: 'max-w-full'
-        //     },
-        //     number: {
-        //         label: 'dark:text-white',
-        //         help: 'dark:text-white',
-        //         input: 'placeholder:text-neutral-500 dark:text-white',
-        //         wrapper: 'max-w-full'
-        //     },
-        //     select: {
-        //         label: 'dark:text-white',
-        //         help: 'dark:text-white',
-        //         input: 'placeholder:text-neutral-500 dark:text-white cursor-pointer',
-        //         option: 'dark:text-black',
-        //         wrapper: 'max-w-full'
-        //     },
-        //     textarea: {
-        //         label: 'dark:text-white',
-        //         help: 'dark:text-white',
-        //         input: 'placeholder:text-neutral-500 dark:text-white',
-        //         wrapper: 'max-w-full'
-        //     },
-        //     file: {
-        //         label: 'dark:text-white',
-        //         help: 'dark:text-white',
-        //         noFiles: 'placeholder:text-neutral-500 dark:text-white',
-        //         wrapper: 'max-w-full'
-        //     }
-        // })
+        classes: generateClasses({
+            global: {
+                wrapper: 'max-w-full'
+            }
+        })
     }
 })
