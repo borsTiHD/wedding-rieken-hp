921d4fe97d5f7873c0457a0356619ee56005fbe9
borsTiHD <bastian.j@gmx.net>
2023-09-17 20:21:06 +0200

    feat: 👷 Adding env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,17 @@
 export default defineNuxtConfig({
     runtimeConfig: {
         // Server-side runtime config
-        firebaseApikey: '',
-        firebaseAuthdomain: '',
-        firebaseProjectid: '',
-        firebaseStoragebucket: '',
-        firebaseMessagingsenderid: '',
-        firebaseAppid: '',
-        firebaseMeasurementid: '',
+        firebaseAdmin: {
+            projectId: process.env.NUXT_FIREBASE_ADMIN_PROJECTID,
+            privateKeyId: process.env.NUXT_FIREBASE_ADMIN_PRIVATE_KEY_ID,
+            privateKey: process.env.NUXT_FIREBASE_ADMIN_PRIVATE_KEY,
+            clientEmail: process.env.NUXT_FIREBASE_ADMIN_CLIENT_EMAIL,
+            clientId: process.env.NUXT_FIREBASE_ADMIN_CLIENT_ID,
+            authUri: process.env.NUXT_FIREBASE_ADMIN_AUTH_URI,
+            tokenUri: process.env.NUXT_FIREBASE_ADMIN_TOKEN_URI,
+            authProviderX509CertUrl: process.env.NUXT_FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+            clientC509CertUrl: process.env.NUXT_FIREBASE_ADMIN_CLIENT_X509_CERT_URL
+        },
         // Client-side runtime config
         public: {
             firebase: {
