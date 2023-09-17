a9b85b6782ea0c24c38664f28184d9aaca9d65b6
borsTiHD <bastian.j@gmx.net>
2023-09-17 11:41:54 +0200

    feat: ✨ Adding logout functionality

---
+++ src/components/user/LogoutButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <Button label="Ausloggen" severity="danger" icon="pi pi-check" :loading="loading" @click="logout" />
+</template>
+
+<script setup lang="ts">
+import { useToast } from 'primevue/usetoast'
+
+// Composables
+const toast = useToast()
+const { logoutUser } = useFirebaseAuth()
+
+// Data
+const loading = ref(false)
+
+// Submit button
+const logout = async() => {
+    loading.value = true
+    await logoutUser()
+        .then(() => {
+            toast.add({
+                severity: 'success',
+                summary: 'Erfolgreich ausgeloggt',
+                detail: 'Sie wurden erfolgreich ausgeloggt.',
+                life: 3000
+            })
+        })
+        .catch((error: { message: string }) => {
+            toast.add({
+                severity: 'error',
+                summary: 'Fehler beim Ausloggen',
+                detail: error.message,
+                life: 3000
+            })
+        })
+        .finally(() => {
+            loading.value = false
+        })
+}
+</script>
--- src/pages/admin/index.vue
+++ src/pages/admin/index.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="m-4 flex flex-col gap-4">
-        <LoginForm />
+        <div class="p-4 border border-gray-400">
+            <LoginForm v-if="!user" />
+            <LogoutButton v-if="user" />
+        </div>
+
         <ShowCountdown :timestamp="config?.weddingDate" />
         <DateDisplay :timestamp="config?.weddingDate" />
         <ChangeTimestamp />
@@ -10,7 +14,8 @@
 
 <script setup lang="ts">
 import { useAppStore } from '@/stores/app'
-import LoginForm from '@/components/login/LoginForm.vue'
+import LoginForm from '@/components/user/LoginForm.vue'
+import LogoutButton from '@/components/user/LogoutButton.vue'
 import ShowCountdown from '@/components/ShowCountdown.vue'
 import DateDisplay from '@/components/DateDisplay.vue'
 import ChangeTimestamp from '@/components/admin/ChangeTimestamp.vue'
@@ -19,4 +24,7 @@ import ApiTesting from '@/components/ApiTesting.vue'
 // App config
 const appStore = useAppStore()
 const config = computed(() => appStore.config)
+
+// User
+const { user } = useFirebaseAuth()
 </script>
