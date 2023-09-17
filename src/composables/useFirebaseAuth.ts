a64da486a7116474c9d81608e9a23d4f069b5ffc
borsTiHD <bastian.j@gmx.net>
2023-09-17 12:01:24 +0200

    feat: 🥅 Error handling optimized

--- src/composables/useFirebaseAuth.ts
+++ src/composables/useFirebaseAuth.ts
@@ -1,4 +1,5 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, User} from 'firebase/auth'
+import { FirebaseError } from '@firebase/util'
 
 export default function() {
     const { $auth } = useNuxtApp() // From firebase.client.ts
@@ -6,44 +7,69 @@ export default function() {
 
     // Register a new user
     const registerUser = async(email: string, password: string): Promise<boolean> => {
-        try {
-            const userCreds = await createUserWithEmailAndPassword($auth, email, password)
-            if (userCreds) {
-                user.value = userCreds.user
-                return true
+        const userCreds = await createUserWithEmailAndPassword($auth, email, password).catch((error: FirebaseError) => {
+            let errorMessage = error.message
+
+            // Handle specific errors
+            if (error.code === 'auth/email-already-in-use') {
+                errorMessage = 'Es existiert bereits ein Benutzer mit dieser E-Mail-Adresse.'
+            } else if (error.code === 'auth/invalid-email') {
+                errorMessage = 'Die E-Mail-Adresse ist ungültig.'
+            } else if (error.code === 'auth/weak-password') {
+                errorMessage = 'Das Passwort ist zu schwach.'
+            } else if (error.code === 'auth/too-many-requests') {
+                errorMessage = 'Zu viele Anfragen. Bitte versuche es später erneut.'
             }
-        } catch (error: unknown) {
+
             console.error(error)
-            if (error instanceof Error) {
-                // handle error
-            }
-            return false
+            throw new Error(errorMessage)
+        })
+
+        // If the user is logged in, set the user state
+        if (userCreds) {
+            user.value = userCreds.user
+            return true
         }
-        return false
+
+        throw new Error('Registrierung fehlgeschlagen - unbekannter Fehler.')
     }
 
     // Login a user
     const loginUser = async(email: string, password: string): Promise<boolean> => {
-        try {
-            const userCreds = await signInWithEmailAndPassword($auth, email, password)
-            if (userCreds) {
-                user.value = userCreds.user
-                return true
+        const userCreds = await signInWithEmailAndPassword($auth, email, password).catch((error: FirebaseError) => {
+            let errorMessage = error.message
+
+            // Handle specific errors
+            if (error.code ==='auth/wrong-password') {
+                errorMessage = 'Das Passwort ist falsch.'
+            } else if (error.code === 'auth/user-not-found') {
+                errorMessage = 'Es existiert kein Benutzer mit dieser E-Mail-Adresse.'
+            } else if (error.code === 'auth/invalid-email') {
+                errorMessage = 'Die E-Mail-Adresse ist ungültig.'
+            } else if (error.code === 'auth/too-many-requests') {
+                errorMessage = 'Zu viele Anfragen. Bitte versuche es später erneut.'
             }
-        } catch (error: unknown) {
-            throw error
+
+            console.error(error)
+            throw new Error(errorMessage)
+        })
+
+        // If the user is logged in, set the user state
+        if (userCreds) {
+            user.value = userCreds.user
+            return true
         }
-        throw new Error('Could not login user - unknown error')
+
+        throw new Error('Login fehlgeschlagen - unbekannter Fehler.')
     }
 
     // Logout a user
     const logoutUser = async(): Promise<void> => {
-        try {
-            await $auth.signOut()
-            user.value = null
-        } catch (error: unknown) {
-            throw error
-        }
+        await $auth.signOut().catch((error: FirebaseError) => {
+            console.error(error)
+            throw new Error('Logout fehlgeschlagen - unbekannter Fehler.')
+        })
+        user.value = null
     }
 
     // Listen to auth state changes
