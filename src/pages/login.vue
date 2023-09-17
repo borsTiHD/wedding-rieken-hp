57530ff8b188ff5310c15e2f552b96fb8a36ceb5
borsTiHD <bastian.j@gmx.net>
2023-09-17 12:31:50 +0200

    feat: ✨ Adding login/logout page

---
+++ src/pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="m-4 flex flex-col gap-4">
+        <LoginForm v-if="!user" />
+        <LogoutButton v-if="user" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import LoginForm from '@/components/user/LoginForm.vue'
+import LogoutButton from '@/components/user/LogoutButton.vue'
+
+// User
+const { user } = useFirebaseAuth()
+</script>
