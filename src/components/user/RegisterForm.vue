c28c2e39cb0e6e2bdd10866b9caa40744700800e
borsTiHD <bastian.j@gmx.net>
2023-09-24 12:51:56 +0200

    feat: ✨ Adding register form/page

---
+++ src/components/user/RegisterForm.vue
@@ -0,0 +1,73 @@
+<template>
+    <FormKit
+        v-slot="{ state: { valid } }"
+        type="form"
+        :actions="false"
+        @submit="handleSubmit"
+    >
+        <div class="flex flex-col gap-4">
+            <FormKit
+                type="email"
+                name="email"
+                label="Email"
+                prefix-icon="email"
+                placeholder="myname@website.com"
+                help="Bitte geben Sie Ihre E-Mail-Adresse ein."
+                validation="required|email"
+                autofocus
+            />
+            <FormKit
+                type="password"
+                name="password"
+                label="Password"
+                prefix-icon="password"
+                help="Bitte geben Sie Ihr Passwort ein."
+                validation="required"
+            />
+
+            <div class="flex gap-2">
+                <Button label="Einloggen" icon="pi pi-check" type="submit" :loading="loading" :disabled="!valid" />
+            </div>
+        </div>
+    </FormKit>
+</template>
+
+<script setup lang="ts">
+import { useToast } from 'primevue/usetoast'
+
+// Composables
+const toast = useToast()
+const { registerUser } = useFirebaseAuth()
+
+// Data
+const loading = ref(false)
+
+// Submit button
+const handleSubmit = async(form: { email: string, password: string }) => {
+    loading.value = true
+
+    // Create user
+    const response = await registerUser(form.email, form.password).catch((error: { message: string }) => {
+        toast.add({
+            severity: 'error',
+            summary: 'Fehler beim Registrieren',
+            detail: error.message,
+            life: 10000
+        })
+        return false
+    })
+
+    // If response is true, user is logged in
+    if (response) {
+        toast.add({
+            severity: 'success',
+            summary: 'User erstellt',
+            detail: 'Der User wurde erfolgreich erstellt.',
+            life: 10000
+        })
+    }
+
+    // Stop loading
+    loading.value = false
+}
+</script>
---
+++ src/pages/register.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="m-4 flex flex-col gap-4">
+        <RegisterForm v-if="!user" />
+        <LogoutUser v-if="user" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import RegisterForm from '@/components/user/RegisterForm.vue'
+import LogoutUser from '@/components/user/LogoutUser.vue'
+import { useUserStore } from '@/stores/user'
+
+// User store
+const userStore = useUserStore()
+const user = computed(() => userStore.user)
+</script>
