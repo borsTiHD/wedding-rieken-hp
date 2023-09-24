35fa4d95b144460a5f17acb467a634188163e6e9
borsTiHD <bastian.j@gmx.net>
2023-09-24 18:48:04 +0200

    feat: ✨ Adding storage from firebase

---
+++ src/composables/useFirebaseStorage.ts
@@ -0,0 +1,42 @@
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import { FirebaseError } from '@firebase/util'
+
+export function useFirebaseStorage() {
+    // From firebase.client.ts
+    const { $storage } = useNuxtApp()
+
+    // Upload a file
+    const uploadFile = async(path: string, file: File): Promise<string> => {
+        // Create a storage reference from our storage service
+        const storageRef = ref($storage, path)
+
+        // Upload file
+        const snapshot = await uploadBytes(storageRef, file).catch((error: FirebaseError) => {
+            let errorMessage = error.message
+
+            // Handle specific errors
+            if (error.code === 'storage/unauthorized') {
+                errorMessage = 'Du hast keine Berechtigung, diese Datei hochzuladen.'
+            } else if (error.code === 'storage/canceled') {
+                errorMessage = 'Der Upload wurde abgebrochen.'
+            } else if (error.code === 'storage/unknown') {
+                errorMessage = 'Ein unbekannter Fehler ist aufgetreten.'
+            }
+
+            console.error(error)
+            throw new Error(errorMessage)
+        })
+
+        // Get download URL
+        const downloadURL = await getDownloadURL(snapshot.ref).catch((error: FirebaseError) => {
+            console.error(error)
+            throw new Error('Die Datei konnte nicht hochgeladen werden.')
+        })
+
+        return downloadURL
+    }
+
+    return {
+        uploadFile
+    }
+}
--- src/plugins/firebase.client.ts
+++ src/plugins/firebase.client.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 export default defineNuxtPlugin(() => {
     const config = useRuntimeConfig()
@@ -9,11 +10,13 @@ export default defineNuxtPlugin(() => {
     const app = initializeApp({ ...firebase })
     const auth = getAuth(app)
     const firestore = getFirestore(app)
+    const storage = getStorage(app)
 
     return {
         provide: {
             auth,
-            firestore
+            firestore,
+            storage
         }
     }
 })
