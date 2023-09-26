09c67b22d2653f1bd116cf8d041be3ac9dd0ce36
borsTiHD <bastian.j@gmx.net>
2023-09-26 10:02:44 +0200

    style: 💡 Changing comments

--- src/server/lib/firebase.ts
+++ src/server/lib/firebase.ts
@@ -2,11 +2,7 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase SDK for server-side rendering
 const config = useRuntimeConfig()
 const { firebase } = config.public // Set in nuxt.config.ts
 
