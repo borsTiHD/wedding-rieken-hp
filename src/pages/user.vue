f2f36241ca4e916a95cfeda6e1510556f71c88cb
borsTiHD <bastian.j@gmx.net>
2023-10-07 11:57:26 +0200

    feat: 🌐 Locals for user view

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -1,12 +1,16 @@
 <template>
     <!-- User not logged in -->
     <div v-if="!uid" class="flex flex-col gap-4">
-        <h1 class="text-2xl">Sie sind nicht eingeloggt:</h1>
-        <span>Klicke <NuxtLink to="login" class="font-medium text-blue-600">hier</NuxtLink> um dich einloggen.</span>
+        <h1 class="text-2xl">{{ t('login.notLoggedIn') }}</h1>
+        <i18n-t keypath="login.loginLink" tag="span">
+            <template #action>
+                <NuxtLink to="login" class="font-medium text-blue-600">{{ t('login.loginLinkTag') }}</NuxtLink>
+            </template>
+        </i18n-t>
     </div>
 
     <!-- User Details -->
-    <div v-else class="bg-gray-100 py-4 flex flex-col gap-4">
+    <div v-else class="py-4 flex flex-col gap-4">
         <div class="w-full sm:w-11/12 md:w-10/12 lg:w-8/12 xl:w-6/12 mx-auto flex flex-col gap-4">
             <!-- User profile picture -->
             <DisplayAvatar />
@@ -21,6 +25,9 @@
 import DisplayAvatar from '@/components/user/DisplayAvatar.vue'
 import DisplayUserprofile from '@/components/user/DisplayUserprofile.vue'
 import { useUserStore } from '@/stores/user'
+
+// Localisation
+const { t } = useI18n()
 
 // User store
 const userStore = useUserStore()
