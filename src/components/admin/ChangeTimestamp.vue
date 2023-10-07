7c95b18ddd2debdf5335b8cdd6cdde582f212fb3
borsTiHD <bastian.j@gmx.net>
2023-10-07 11:48:05 +0200

    feat: 🌐 Locals for timestamp component

--- src/components/admin/ChangeTimestamp.vue
+++ src/components/admin/ChangeTimestamp.vue
@@ -2,13 +2,13 @@
     <div class="flex gap-4">
         <span class="p-input-icon-right">
             <i class="pi pi-calendar" />
-            <Calendar v-model="date" date-format="dd.mm.yy" :minDate="minDate" />
+            <Calendar v-model="date" :date-format="t('general.inputDateFormat')" :minDate="minDate" />
         </span>
         <span class="p-input-icon-right">
             <i class="pi pi-clock" />
             <Calendar v-model="time" timeOnly />
         </span>
-        <Button type="submit" label="Submit" @click="handleSubmit" />
+        <Button type="submit" :label="t('buttons.submit')" @click="handleSubmit" />
     </div>
 </template>
 
@@ -18,6 +18,7 @@ import { useAppStore } from '@/stores/app'
 
 // Composables
 const toast = useToast()
+const { t } = useI18n()
 const { update } = useFirestore() // Firestore composable
 
 // App config
@@ -33,14 +34,24 @@ const minDate = ref(new Date())
 const handleSubmit = async() => {
     // Check if date is selected
     if (!date.value) {
-        toast.add({ severity: 'error', summary: 'Error', detail: 'Kein Datum ausgewählt.', life: 5000 })
-        throw new Error('No date selected')
+        toast.add({
+            severity: 'error',
+            summary: t('admin.changeDate.error'),
+            detail: t('admin.changeDate.errorNoDate'),
+            life: 5000
+        })
+        throw new Error(t('admin.changeDate.errorNoDate'))
     }
 
     // Check if time is selected
     if (!time.value) {
-        toast.add({ severity: 'error', summary: 'Error', detail: 'Keine Uhrzeit ausgewählt.', life: 5000 })
-        throw new Error('No time selected')
+        toast.add({
+            severity: 'error',
+            summary: t('admin.changeDate.error'),
+            detail: t('admin.changeDate.errorNoTime'),
+            life: 5000
+        })
+        throw new Error(t('admin.changeDate.errorNoTime'))
     }
 
     // Combine date and time
@@ -54,12 +65,22 @@ const handleSubmit = async() => {
 
     // Update wedding date
     await update('app', 'config', 'weddingDate', timestamp).catch((error: { message: string }) => {
-        toast.add({ severity: 'error', summary: 'Error', detail: error.message, life: 5000 })
+        toast.add({
+            severity: 'error',
+            summary: t('admin.changeDate.error'),
+            detail: error.message,
+            life: 5000
+        })
         throw new Error(error.message)
     })
 
     // Show success message
-    toast.add({ severity: 'success', summary: 'Success', detail: 'Wedding date updated', life: 5000 })
+    toast.add({
+        severity: 'success',
+        summary: t('admin.changeDate.success'),
+        detail: t('admin.changeDate.successDetail'),
+        life: 5000
+    })
 
     // Update app config
     appStore.fetchConfig()
@@ -67,7 +88,7 @@ const handleSubmit = async() => {
 
 onMounted(() => {
     if (!config?.weddingDate) {
-        console.error('No wedding date found')
+        console.error(t('admin.changeDate.errorNoDateFound'))
         return false
     }
 
