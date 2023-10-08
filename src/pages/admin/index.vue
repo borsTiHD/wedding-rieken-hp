30d8ff55b4773e1d17dfe48dae113541d13a1b8f
borsTiHD <bastian.j@gmx.net>
2023-10-08 12:05:01 +0200

    feat: 🎨 Redesigning admin page for future updates

--- src/pages/admin/index.vue
+++ src/pages/admin/index.vue
@@ -1,47 +1,45 @@
 <template>
-    <div class="m-4 flex flex-col gap-4">
-        <!-- You are not logged in -->
-        <div class="p-4 border border-gray-400">
-            <LoginForm v-if="!user.uid" />
-            <LogoutUser v-else />
-        </div>
+    <!-- You are an admin -->
+    <div v-if="user.uid && userProfile?.role === 'admin'" class="flex flex-col gap-4">
+        <!-- Change date -->
+        <Card>
+            <template #title>{{ t('admin.listUsers.header') }}</template>
+            <template #content>
+                <div class="p-2 flex flex-col gap-4">
+                    <div class="flex gap-4">
+                        <ChangeTimestamp />
+                        <DateDisplay :timestamp="config?.weddingDate" />
+                    </div>
+                    <ShowCountdown :timestamp="config?.weddingDate" />
+                </div>
+            </template>
+        </Card>
 
-        <!-- You are an admin -->
-        <div v-if="user.uid && userProfile?.role === 'admin'" class="flex flex-col gap-4">
-            <div class="flex gap-4">
-                <ChangeTimestamp />
-                <DateDisplay :timestamp="config?.weddingDate" />
-            </div>
-            <ShowCountdown :timestamp="config?.weddingDate" />
+        <!-- Create User modal -->
+        <DisplayModal
+            ref="createUserModal"
+            :header="t('admin.createUser.header')"
+            buttonClass="basis-1/2"
+            :buttonLabel="t('admin.createUser.buttonLabel')"
+            buttonIcon="pi pi-user-plus"
+            button
+        >
+            <template #content>
+                <CreateUser @created="createUserModal?.close()" />
+            </template>
+        </DisplayModal>
 
-            <!-- Create User modal -->
-            <DisplayModal
-                ref="createUserModal"
-                :header="t('admin.createUser.header')"
-                buttonClass="basis-1/2"
-                :buttonLabel="t('admin.createUser.buttonLabel')"
-                buttonIcon="pi pi-user-plus"
-                button
-            >
-                <template #content>
-                    <CreateUser @created="createUserModal?.close()" />
-                </template>
-            </DisplayModal>
+        <ListUsers />
+    </div>
 
-            <ListUsers />
-        </div>
-
-        <!-- You are not an admin -->
-        <div v-else-if="user.uid">
-            <h1 class="text-2xl text-center whitespace-pre-line">{{ t('user.userRole.notAdmin') }}</h1>
-        </div>
+    <!-- You are not an admin -->
+    <div v-else>
+        <h1 class="text-2xl text-center whitespace-pre-line">{{ t('user.userRole.notAdmin') }}</h1>
     </div>
 </template>
 
 <script setup lang="ts">
 import DisplayModal from '@/components/DisplayModal.vue'
-import LoginForm from '@/components/user/LoginForm.vue'
-import LogoutUser from '@/components/user/LogoutUser.vue'
 import ShowCountdown from '@/components/ShowCountdown.vue'
 import DateDisplay from '@/components/DateDisplay.vue'
 import ChangeTimestamp from '@/components/admin/ChangeTimestamp.vue'
