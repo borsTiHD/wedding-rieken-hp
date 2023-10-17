2be1a6192ceaeeecf63d87733c102934c9e9c692
borsTiHD <bastian.j@gmx.net>
2023-10-17 13:22:05 +0200

    feat: 🚧 Adding new admin module for creating a qrcode with an invite link - still wip

---
+++ src/components/admin/ShowQRCode.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="flex flex-col">
+        <div class="flex justify-center">
+            <Skeleton v-if="loading" width="10rem" height="10rem" />
+            <canvas :id="canvasId" />
+        </div>
+        <!-- Download button -->
+        <Button
+            :label="t('admin.qrcode.download')"
+            icon="pi pi-download"
+            raised
+            @click="downloadQRCode"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import QRCode from 'qrcode'
+import { useUserStore } from '@/stores/user'
+
+// Composables
+const { t } = useI18n()
+const { queryByCollectionAndId } = useFirestore()
+
+// User store
+const userStore = useUserStore()
+const user = computed(() => userStore.user)
+const userProfile = computed(() => userStore.userProfile)
+
+// Data
+const loading = ref(false)
+const canvasId = 'invitationTokenCanvas'
+const invitationToken = ref()
+
+// Create QR code from token
+// Returns a promise and resolves true if successful
+const createQRCode = async() => {
+    return new Promise((resolve, reject) => {
+        // TODO: Check later if the url is correct and valid
+
+        // Create url for QR code
+        const baseUrl = window.location.origin
+        const url = `${baseUrl}/?token=${invitationToken.value}`
+
+        // Create QR code
+        const canvas = document.getElementById(canvasId) as HTMLCanvasElement
+        const errorCorrectionLevel = 'M' // L, M, Q, H - default: M - https://github.com/soldair/node-qrcode#error-correction-level
+        QRCode.toCanvas(canvas, url, { errorCorrectionLevel, color: { dark: '#000000ff', light: '#ffffffff' } }, (error: Error) => {
+            if (error) {
+                console.error(error)
+                reject(error)
+            } else {
+                resolve(true)
+            }
+        })
+    })
+}
+
+// Download QR code
+const downloadQRCode = () => {
+    const canvas = document.getElementById(canvasId) as HTMLCanvasElement
+    const link = document.createElement('a')
+    link.download = 'inviteQrCode.png'
+    link.href = canvas.toDataURL()
+    link.click()
+}
+
+// Get invite token
+const getInviteToken = async() => {
+    // Check if user is logged in
+    if (!user.value) {
+        throw new Error(t('firebase.custom.noUserLoggedIn'))
+    }
+
+    // Check if user is admin
+    // This is not necessary, because the button is only visible for admins also the API will throw an error if the user is not admin
+    if (userProfile.value && userProfile.value.role !== 'admin') {
+        throw new Error(t('admin.notAdminError'))
+    }
+
+    // If collection and id is specified, return document from collection
+    const response = await queryByCollectionAndId('app', 'admin').catch((error) => {
+        const errorMessage = handleFirebaseError(error, 'admin.inviteToken.error')
+        throw new Error(errorMessage)
+    })
+
+
+    // Throw error if no response
+    if (!response) {
+        throw new Error(t('admin.inviteToken.error'))
+    }
+
+    // Set token
+    invitationToken.value = response.inviteToken as string
+
+    return true
+}
+
+// On mounted
+onMounted(async() => {
+    loading.value = true
+    const checkToken = await getInviteToken().catch((error) => {
+        console.error(error)
+        return false
+    })
+
+    // If token is not valid, return
+    if (!checkToken) {
+        loading.value = false
+        return false
+    }
+
+    // Create QR code if token is valid
+    await createQRCode()
+    loading.value = false
+})
+</script>
--- src/pages/admin/index.vue
+++ src/pages/admin/index.vue
@@ -1,19 +1,27 @@
 <template>
     <!-- You are an admin -->
     <div v-if="user.uid && userProfile?.role === 'admin'" class="flex flex-col gap-4">
-        <div class="flex gap-4 flex-wrap">
-            <!-- Change date -->
-            <Card class="grow">
-                <template #title>{{ t('admin.changeDate.header') }}</template>
-                <template #content>
-                    <div class="p-2 flex flex-col gap-4">
-                        <ChangeTimestamp />
-                        <div class="flex gap-4">
-                            <span class="text-2xl">{{ t('admin.changeDate.label') }}:</span>
-                            <DateDisplay :timestamp="config?.weddingDate" />
-                        </div>
-                        <ShowCountdown :timestamp="config?.weddingDate" />
+        <!-- Change date -->
+        <Card class="grow">
+            <template #title>{{ t('admin.changeDate.header') }}</template>
+            <template #content>
+                <div class="flex flex-col gap-4">
+                    <ChangeTimestamp />
+                    <div class="flex gap-4">
+                        <span class="text-2xl">{{ t('admin.changeDate.label') }}:</span>
+                        <DateDisplay :timestamp="config?.weddingDate" />
                     </div>
+                    <ShowCountdown :timestamp="config?.weddingDate" />
+                </div>
+            </template>
+        </Card>
+
+        <div class="flex gap-4 flex-wrap">
+            <!-- QR Code -->
+            <Card class="grow basis-96" :pt="{ content: { class: 'p-0' } }">
+                <template #title>{{ t('admin.qrcode.header') }}</template>
+                <template #content>
+                    <ShowQRCode />
                 </template>
             </Card>
 
@@ -21,9 +29,7 @@
             <Card class="grow basis-96">
                 <template #title>{{ t('admin.inviteToken.header') }}</template>
                 <template #content>
-                    <div class="p-2 flex flex-col gap-4">
-                        <ChangeInviteToken />
-                    </div>
+                    <ChangeInviteToken />
                 </template>
             </Card>
         </div>
@@ -41,6 +47,7 @@
 <script setup lang="ts">
 import ShowCountdown from '@/components/ShowCountdown.vue'
 import DateDisplay from '@/components/DateDisplay.vue'
+import ShowQRCode from '@/components/admin/ShowQRCode.vue'
 import ChangeTimestamp from '@/components/admin/ChangeTimestamp.vue'
 import ChangeInviteToken from '@/components/admin/ChangeInviteToken.vue'
 import ListUsers from '@/components/admin/ListUsers.vue'
