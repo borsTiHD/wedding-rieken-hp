6976e96fc5b82c0fe539b1262ff9a60959207079
borsTiHD <bastian.j@gmx.net>
2023-10-21 15:38:09 +0200

    feat: 🚨 Linting exception for single name component

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,7 @@ module.exports = {
 
         // Vue specific rules
         'vue/multi-word-component-names': ['error', {
-            ignores: ['default', 'index', '[name]', 'admin', 'login', 'register', 'user']
+            ignores: ['default', 'index', '[name]', 'admin', 'login', 'register', 'user', 'profile']
         }],
         'vue/max-attributes-per-line': 'off',
         'vue/singleline-html-element-content-newline': 'off',
