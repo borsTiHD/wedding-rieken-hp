da8ec77a1a0cd3ae33ac908823eb6b2b2886b568
borsTiHD <bastian.j@gmx.net>
2023-10-26 12:36:48 +0200

    feat: ✨ Adding a card for the content. Also added a TabPanel for the choose to register without a password.

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,17 +1,29 @@
 <template>
-    <div v-if="!uid" class="flex flex-col gap-4">
-        <h1 class="text-2xl">{{ t('register.header') }}:</h1>
-        <RegisterForm />
-    </div>
+    <Card>
+        <template #content>
+            <div v-if="!uid" class="flex flex-col gap-4">
+                <h1 class="text-2xl">{{ t('register.header') }}:</h1>
+                <TabView v-model:activeIndex="activeIndex">
+                    <TabPanel :header="t('register.header')">
+                        <RegisterForm />
+                    </TabPanel>
+                    <TabPanel :header="t('login.wihtoutPassword')">
+                        <LoginWithEmailLink />
+                    </TabPanel>
+                </TabView>
+            </div>
 
-    <div v-else class="flex flex-col gap-4">
-        <h1 class="text-2xl">{{ t('login.alreadyLoggedIn') }}</h1>
-        <LogoutUser />
-    </div>
+            <div v-else class="flex flex-col gap-4">
+                <h1 class="text-2xl">{{ t('login.alreadyLoggedIn') }}:</h1>
+                <LogoutUser />
+            </div>
+        </template>
+    </Card>
 </template>
 
 <script setup lang="ts">
 import RegisterForm from '@/components/user/RegisterForm.vue'
+import LoginWithEmailLink from '@/components/user/LoginWithEmailLink.vue'
 import LogoutUser from '@/components/user/LogoutUser.vue'
 import { useUserStore } from '@/stores/user'
 
@@ -21,4 +33,7 @@ const { t } = useI18n()
 // User store
 const userStore = useUserStore()
 const uid = computed(() => userStore.uid)
+
+// Tab Menu
+const activeIndex = ref(0)
 </script>
