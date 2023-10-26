f2afa5bbe173b9b15e82c42bdbec10ca8e3b5764
borsTiHD <bastian.j@gmx.net>
2023-10-26 10:46:37 +0200

    feat: 💄 User have a completion knob in their profile to see if something is missing, it links directly to the profile stepper.

--- src/components/user/DisplayProfileProgress.vue
+++ src/components/user/DisplayProfileProgress.vue
@@ -1,16 +1,49 @@
 <template>
-    <Knob
-        v-model="value"
-        v-tooltip.left="'Profil vervollständigen'"
-        aria-label="Profile completion progress"
-        :size="50"
-        readonly
-    />
+    <NuxtLink :to="profileCompletionLink">
+        <Knob
+            v-model="checkState"
+            v-tooltip.left="t('user.profileProgress.tooltip')"
+            :valueColor="color"
+            aria-label="Profile completion progress"
+            :size="50"
+            readonly
+        />
+    </NuxtLink>
 </template>
 
 <script setup lang="ts">
-const value = ref(66)
+const { allChecks } = useProfileChecker()
 
-// TODO: Add a completion check for every state and make a progress circle
-// Also add a linkt to the profile completion stepper if the profile is not complete
+// Localisation
+const { t } = useI18n()
+const localePath = useLocalePath()
+
+// Refs
+const profileCompletionLink = computed(() => localePath('/user/profile/'))
+
+// Checks the state of the profile
+// For that, it loops through all checks and returns a percentage of completion
+const checkState = computed(() => {
+    const totalChecks = allChecks.length
+    let completedChecks = 0
+
+    for (const check of allChecks) {
+        if (check.check()) {
+            completedChecks++
+        }
+    }
+
+    return Math.round((completedChecks / totalChecks) * 100)
+})
+
+// Color of the knob based on the state
+const color = computed(() => {
+    if (checkState.value === 100) {
+        return 'green'
+    } else if (checkState.value >= 50) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+})
 </script>
