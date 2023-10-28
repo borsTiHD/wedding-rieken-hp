18879a64812a777bbab9a6e5a1d04cd164d4b40b
borsTiHD <bastian.j@gmx.net>
2023-10-28 15:00:29 +0200

    feat: 💄 Background for loaded changed

--- src/components/LoadingOverlay.vue
+++ src/components/LoadingOverlay.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-opacity-70 bg-gray-800 z-50">
+    <div class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-opacity-70 bg-gray-100 z-50">
         <div class="relative h-16 w-16">
             <div class="animate-spin rounded-full h-full w-full border-t-4 border-blue-500" />
-            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white">
+            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-600">
                 {{ props.progress }}%
             </div>
         </div>
