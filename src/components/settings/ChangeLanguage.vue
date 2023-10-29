97a71a70781ec214c0e9ca312f5d93a97e478de7
borsTiHD <bastian.j@gmx.net>
2023-10-29 11:42:22 +0100

    feat: ✨ New and improved language selector

--- src/components/settings/ChangeLanguage.vue
+++ src/components/settings/ChangeLanguage.vue
@@ -1,33 +1,40 @@
 <template>
     <div class="flex">
-        <!-- :label="t('settings.language')" -->
-        <FormKit
-            v-model="locale"
-            type="select"
-            name="language"
-            outerClass="!m-0"
-            :options="availableLocales"
-            @change="onLanguageChange"
-        />
+        <Dropdown v-model="locale" :options="availableLocales" optionLabel="label" optionValue="code" @change="onLanguageChange">
+            <template #value="slotProps">
+                <div v-if="slotProps.value" class="flex align-items-center gap-2">
+                    <span :class="`fi fi-${slotProps.value}`" />
+                    <!-- <div>{{ slotProps.value.label }}</div> -->
+                </div>
+                <span v-else>-</span>
+            </template>
+            <template #option="slotProps">
+                <div class="flex align-items-center gap-2">
+                    <span :class="`fi fi-${slotProps.option.code}`" />
+                    <div>{{ slotProps.option.label }}</div>
+                </div>
+            </template>
+        </Dropdown>
     </div>
 </template>
 
 <script setup lang="ts">
+import type { DropdownChangeEvent } from 'primevue/dropdown'
+
 // Language options
 const { locale, locales, setLocale, setLocaleCookie } = useI18n()
 const availableLocales = computed(() => {
     return locales.value.map((locale) => {
         return {
-            value: typeof locale === 'string' ? locale : locale.code,
+            code: typeof locale === 'string' ? locale : locale.code,
             label: typeof locale === 'string' ? locale : locale.name
-        } as { value: string; label: string }
+        } as { code: string; label: string }
     })
 })
 
 // On language change
-const onLanguageChange = (event: Event) => {
-    const target = event.target as HTMLSelectElement
-    const selectedLocale = target.value
+const onLanguageChange = (event: DropdownChangeEvent) => {
+    const selectedLocale = event.value
     setLocaleCookie(selectedLocale)
     setLocale(selectedLocale)
 }
