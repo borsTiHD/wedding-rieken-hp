01c9fb4cd4969e6ef2bf6f2d7a9b7cb6d0f8a04c
borsTiHD <bastian.j@gmx.net>
2023-10-29 10:21:15 +0100

    feat: 💄 Fixing navbar styling with new theme and adding enter keypress for navigation

--- src/components/layout/AppNavbar.vue
+++ src/components/layout/AppNavbar.vue
@@ -1,17 +1,18 @@
 <template>
-    <Menubar :model="navItems" class="bg-transparent border-none">
+    <Menubar :model="navItems" class="!bg-transparent !border-none">
         <template #item="{ item }">
             <NuxtLink v-slot="{ href, navigate, isActive, isExactActive }" :to="item.path" custom>
                 <a
                     :href="href"
                     :class="[
-                        'p-menuitem-link bg-transparent text-black/60', // default
+                        'p-menuitem-link text-black/60', // default
                         'hover:text-primary/80', // hover - hover:text-black/80
                         {
                             'active-link text-primary font-bold': isActive, // active - text-black/60
                             'active-link-exact': isExactActive // active-exact
                         }]"
                     @click="navigate"
+                    @keydown.enter="navigate"
                 >
                     {{ item.name }}
                 </a>
