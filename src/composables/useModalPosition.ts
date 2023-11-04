0b9dc3d5cf1db1774e043143c9d2ec9b114a57cb
borsTiHD <bastian.j@gmx.net>
2023-11-04 10:00:47 +0100

    feat: 🎨 Improving modal position and change the value if the size changes, also prefent multiple calls (even the position didn't updated, the size event was called every time)

--- src/composables/useModalPosition.ts
+++ src/composables/useModalPosition.ts
@@ -2,11 +2,13 @@ import { useWindowSize } from '@/composables/useWindowSize'
 
 // Composable determines the modal position based on the window size
 export function useModalPosition() {
-    const { windowWidth } = useWindowSize() // Window Size composable
-    const isMobile = windowWidth.value < 640 // Mobile breakpoint
+    // Window Size composable with a big timer to prevent multiple calls
+    // 1000 * 10 = 10 seconds
+    const { windowWidth } = useWindowSize(1000 * 10)
 
     // Computed propertie for the modal position
     const modalPosition = computed<'center' | 'top'>(() => {
+        const isMobile = windowWidth.value < 640 // Mobile breakpoint
         if (isMobile) return 'top'
         return 'center'
     })
