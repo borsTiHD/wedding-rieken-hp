a40a01fc35e0c32018b454bafa7183edbb3beee1
borsTiHD <bastian.j@gmx.net>
2023-11-10 12:46:57 +0100

    feat: 💄 Improved the footer - overlap the footer above the background image if the user scroll below the scrollable space (like on iOS devices)

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
-    <footer class="mt-auto flex justify-center items-center h-56 bg-footer text-primary z-50">
-        <div class="flex flex-col justify-center items-center gap-2">
+    <footer ref="footer" class="mt-auto flex justify-center items-center h-56 bg-footer text-primary z-50">
+        <div class="flex flex-col justify-center items-center gap-2 z-50">
             <span>{{ t('footer.description', { bride, groom }) }}</span>
             <div class="flex gap-2">
                 <template
@@ -21,12 +21,20 @@
                 <NuxtLink :to="githubRepo" target="_blank" class="link link-hover hover:text-rose-500"><i class="pi pi-github" /></NuxtLink>
             </p>
         </div>
+
+        <!-- Background overlay - used to overlay the background image -->
+        <div
+            ref="backgroundOverlay"
+            class="bg-footer fixed w-full bottom-0 left-0 z-0 pointer-events-none"
+            :style="{ height: `${backgroundOverlayHeight}px` }"
+        />
     </footer>
 </template>
 
 <script setup lang="ts">
 import { usePagesStore } from '@/stores/pages'
 import { useAppStore } from '@/stores/app'
+import { useEventListener } from '@/composables/useEventListener'
 
 // Localisation
 const { t } = useI18n()
@@ -42,4 +50,31 @@ const groom = computed(() => appStore.groom)
 
 // Github repo link
 const githubRepo = 'https://github.com/borsTiHD/wedding-rieken-hp/'
+
+// Scroll event listener - calculate the height of the background overlay
+const backgroundOverlayHeight = ref(0)
+useEventListener(window, 'scroll', () => { backgroundOverlayHeight.value = calculateHeight() })
+
+// Calculate the height of the background overlay
+// This function is called on scroll event
+const footer = ref<HTMLElement | null>(null)
+const calculateHeight = () => {
+    if (footer.value) {
+        const rect = footer.value.getBoundingClientRect()
+        const scrollPosition = window.innerHeight - rect.top
+
+        // If the height is greater than 50,
+        // reduce it by half the height of the footer
+        if (scrollPosition > 50) {
+            return scrollPosition - (rect.height / 2)
+        }
+
+        // If the height is greater than 0,
+        // return the height
+        return scrollPosition > 0 ? scrollPosition : 0
+    }
+
+    // If the footer is not set, return 0
+    return 0
+}
 </script>
