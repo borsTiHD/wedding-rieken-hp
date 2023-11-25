d35f0b5d03e51646a09c0962ce71e2cc8aff0153
borsTiHD <bastian.j@gmx.net>
2023-11-25 22:26:07 +0100

    feat: 💄 Adding a scale transition for the profile picture

--- src/components/settings/UserQuickMenu.vue
+++ src/components/settings/UserQuickMenu.vue
@@ -5,7 +5,7 @@
             :image="photoURL ? photoURL : undefined"
             :icon="photoURL ? undefined : 'pi pi-user'"
             tabindex="0"
-            class="cursor-pointer !rounded-full hover:shadow-lg transition-shadow duration-300 ease-in-out"
+            class="cursor-pointer !rounded-full hover:scale-110 hover:shadow-lg transition-all duration-300 ease-in-out"
             size="large"
             shape="circle"
             @click="showSearchPanel"
