656c0abcb47781381687c921ce71c1ea56c7220a
borsTiHD <bastian.j@gmx.net>
2023-11-27 11:40:11 +0100

    feat: 🎨 Improved structure for the user management page

--- src/pages/admin/user-management.vue
+++ src/pages/admin/user-management.vue
@@ -1,10 +1,7 @@
 <template>
     <main class="mx-auto flex flex-wrap gap-4 p-4 w-full sm:w-11/12 md:w-10/12 lg:w-8/12">
-        <!-- You are an admin -->
-        <template v-if="user.uid && userProfile?.role === 'admin'">
-            <!-- List users datatable -->
-            <ListUsers class="basis-full max-w-full" />
-        </template>
+        <!-- List users datatable -->
+        <ListUsers v-if="user.uid && userProfile?.role === 'admin'" class="basis-full max-w-full" />
 
         <!-- You are not an admin -->
         <NoAdmin v-else />
