e29ae2296d4a39a957898f259e3986ed224a4462
borsTiHD <bastian.j@gmx.net>
2024-02-27 19:35:22 +0100

    feat: ✨ Adding a scoreboard and let the game start by hitting the first ghost

--- src/components/ghost/GhostsGame.vue
+++ src/components/ghost/GhostsGame.vue
@@ -1,5 +1,6 @@
 <template>
-    <div ref="ghostContainer" class="absolute inset-0 w-full h-full bg-black/50 z-40">
+    <div ref="ghostContainer" class="absolute inset-0 flex justify-center w-full h-full bg-black/50 z-40">
+        <ScoreBoard :score="highScore" :timer="currentGameTime" />
         <SingleGhost
             v-for="(ghost, index) in ghosts"
             :ref="el => ghost.itemRef = el"
@@ -12,13 +13,14 @@
                 duration: ghost.duration,
                 ghost: ghost.ghost
             }"
-            @hit="hit(ghost)"
+            @hit="hit"
         />
     </div>
 </template>
 
 <script setup lang="ts">
 import SingleGhost from '@/components/ghost/SingleGhost.vue'
+import ScoreBoard from '@/components/ghost/ScoreBoard.vue'
 import type { GhostSetting } from '@/components/ghost/SingleGhost.vue'
 
 interface Ghost extends GhostSetting {
@@ -26,13 +28,62 @@ interface Ghost extends GhostSetting {
     itemRef: Element | ComponentPublicInstance | null
 }
 
+// Game boundaries
 const ghostContainer = ref<HTMLElement | null>(null)
 const containerWidth = ref<number>(ghostContainer.value?.clientWidth ?? 0)
 const containerHeight = ref<number>(ghostContainer.value?.clientHeight ?? 0)
+
+// Game state
+const running = ref<boolean>(false)
 const highScore = ref<number>(0)
+const maxGameTime = 20 // in seconds
+const currentGameTime = ref<number>(0)
 
 const ghosts = ref<Ghost[]>([])
 
+// Game start
+const gameStart = () => {
+    // Reset the game
+    gameReset()
+
+    // Set the game to running state and reset the game time
+    running.value = true
+
+    // Add initial 10 ghosts
+    for (let i = 0; i < 10; i++) {
+        addingGhost()
+    }
+
+    // Start the game timer
+    const timer = setInterval(() => {
+        currentGameTime.value--
+
+        if (currentGameTime.value <= 0) {
+            running.value = false
+            clearInterval(timer)
+
+            // Remove all ghosts
+            ghosts.value = []
+
+            // Add initial ghost
+            // Hitting the first ghost will start the game
+            addingGhost()
+
+            console.log('Game over - Score:', highScore.value)
+        }
+    }, 1000)
+}
+
+// Reset the game
+const gameReset = () => {
+    running.value = false
+    highScore.value = 0
+    currentGameTime.value = maxGameTime
+    ghosts.value = [] // Remove all ghosts
+    addingGhost() // Add initial ghost
+}
+
+// Add a ghost
 const addingGhost = (number?: number) => {
     // Create a random id
     const randomId = Math.floor(Math.random() * 1000)
@@ -52,11 +103,25 @@ const addingGhost = (number?: number) => {
     ghosts.value.push(ghost)
 }
 
+// Ghost hit
+const hit = () => {
+    // If the game is not running,
+    // hitting the first ghost will start the game
+    if (!running.value) {
+        gameStart()
+    }
+
+    // Increase the high score
+    highScore.value++
+}
+
+// Set container sizes
 const setSizes = () => {
     containerWidth.value = ghostContainer.value?.clientWidth ?? 0
     containerHeight.value = ghostContainer.value?.clientHeight ?? 0
 }
 
+// Set new sizes and update the ghosts boundaries
 const setNewSizes = () => {
     setSizes()
     ghosts.value.forEach((ghost) => {
@@ -65,22 +130,12 @@ const setNewSizes = () => {
     })
 }
 
-// Ghost hit
-const hit = (ghost: Ghost) => {
-    // Increase the high score
-    highScore.value++
-
-    console.log('Hit', ghost.id)
-    console.log('High score', highScore.value)
-}
-
 onMounted(() => {
-    setSizes() // Set initial sizes
+    // Set initial sizes
+    setSizes()
 
-    // Add initial 10 ghosts
-    for (let i = 0; i < 10; i++) {
-        addingGhost()
-    }
+    // Reset the game
+    gameReset()
 
     // On window resize, set sizes
     window.addEventListener('resize', setNewSizes)
---
+++ src/components/ghost/ScoreBoard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-if="show" class="relative mb-auto mt-8 p-4 bg-white rounded-xl flex flex-col justify-between items-center gap-2">
+        <span class="text-2xl">Score: {{ props.score }}</span>
+        <span class="text-2xl">Time: {{ props.timer }}</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    score: number,
+    timer: number // in seconds
+}>()
+
+const show = computed(() => props.score > 0 && props.timer > 0)
+</script>
