b58bef282d611bfc5a1c7dacbe0910994c78e478
borsTiHD <bastian.j@gmx.net>
2024-02-28 09:19:23 +0100

    feat: 💄 Changing font weight for scoreboard

--- src/components/ghost/ScoreBoard.vue
+++ src/components/ghost/ScoreBoard.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="show" class="relative mb-auto mt-8 p-4 bg-white rounded-xl flex flex-col justify-between items-center gap-2 select-none pointer-events-none">
-        <span class="text-2xl">{{ t('ghostGame.secret') }}</span>
+        <span class="text-2xl font-bold">{{ t('ghostGame.secret') }}</span>
         <span class="text-2xl">{{ t('ghostGame.score') }}: {{ props.score }}</span>
         <span class="text-2xl">{{ t('ghostGame.time') }}: {{ props.timer }}</span>
     </div>
