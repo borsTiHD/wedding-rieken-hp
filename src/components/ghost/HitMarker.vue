c42f524474a967f1a148eab377d7667cd90f7f81
borsTiHD <bastian.j@gmx.net>
2024-02-28 11:35:05 +0100

    feat: 💄 Changed hitmarker animation

--- src/components/ghost/HitMarker.vue
+++ src/components/ghost/HitMarker.vue
@@ -1,7 +1,7 @@
 <template>
     <span
         v-if="show"
-        class="absolute wobble-hor-bottom bg-black/50 text-white p-2 rounded-md select-none pointer-events-none"
+        class="absolute animate-bounce bg-black/50 text-white p-2 rounded-md select-none pointer-events-none"
         :style="{
             left: left + 'px',
             top: top + 'px',
