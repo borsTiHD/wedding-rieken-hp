fd5319f8fd58c9148703c90af41ed769304a1219
borsTiHD <bastian.j@gmx.net>
2024-02-28 09:18:38 +0100

    feat: ✨ GameOver showing Highscore

--- src/components/ghost/GameOver.vue
+++ src/components/ghost/GameOver.vue
@@ -1,6 +1,7 @@
 <template>
-    <div v-if="show" class="relative mb-auto mt-8 p-4 bg-white rounded-xl flex flex-col justify-between items-center gap-2 select-none pointer-events-none">
-        <span class="text-2xl">{{ t('ghostGame.gameover') }}</span>
+    <div v-if="show" class="relative m-auto p-4 bg-white rounded-xl flex flex-col justify-between items-center gap-2 select-none pointer-events-none">
+        <span class="text-3xl font-bold">{{ t('ghostGame.gameover') }}</span>
+        <span class="text-2xl">{{ t('ghostGame.highscore') }}: {{ props.highscore }}</span>
         <span class="text-2xl">{{ t('ghostGame.score') }}: {{ props.score }}</span>
     </div>
 </template>
@@ -9,6 +10,7 @@
 const props = defineProps<{
     show: boolean
     score: number
+    highscore: number
 }>()
 
 // Localisation
