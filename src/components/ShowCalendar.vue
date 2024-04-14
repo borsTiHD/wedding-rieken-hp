6285d3c22a0625a128ed50a97d2bfe27a820ab0e
borsTiHD <bastian.j@gmx.net>
2024-04-14 10:39:56 +0200

    feat: ✨ Finishing calendar component, displaying month of the given timestamp and marking that specific day

--- src/components/ShowCalendar.vue
+++ src/components/ShowCalendar.vue
@@ -1,12 +1,39 @@
 <template>
-    <div class="w-72 mt-10 text-center">
+    <div class="w-72 text-center">
         <div class="flex-auto text-sm font-semibold text-gray-900">{{ month }} {{ year }}</div>
-        <div class="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500">
+        <div class="mt-2 grid grid-cols-7 text-xs leading-6 text-gray-500">
             <div v-for="day in dayNames" :key="day.long">{{ day.short }}</div>
         </div>
         <div v-if="days" class="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
-            <button v-for="(day, dayIdx) in days" :key="day.date" type="button" :class="['py-1.5 hover:bg-gray-100 focus:z-10', day.isCurrentMonth ? 'bg-white' : 'bg-gray-50', (day.isSelected || day.isToday) && 'font-semibold', day.isSelected && 'text-white', !day.isSelected && day.isCurrentMonth && !day.isToday && 'text-gray-900', !day.isSelected && !day.isCurrentMonth && !day.isToday && 'text-gray-400', day.isToday && !day.isSelected && 'text-indigo-600', dayIdx === 0 && 'rounded-tl-lg', dayIdx === 6 && 'rounded-tr-lg', dayIdx === days.length - 7 && 'rounded-bl-lg', dayIdx === days.length - 1 && 'rounded-br-lg']">
-                <time :datetime="day.date" :class="['mx-auto flex h-7 w-7 items-center justify-center rounded-full', day.isSelected && day.isToday && 'bg-indigo-600', day.isSelected && !day.isToday && 'bg-gray-900']">{{ day.date.split('-').pop().replace(/^0/, '') }}</time>
+            <button
+                v-for="(day, dayIdx) in days"
+                :key="day.date"
+                type="button"
+                tabindex="-1"
+                :class="[
+                    'cursor-default', // base styles
+                    'py-1.5 hover:bg-gray-100 focus:z-10', day.isCurrentMonth ? 'bg-white' : 'bg-gray-50', // current month or not
+                    (day.isSelected || day.isToday) && 'font-semibold', // selected or today
+                    day.isSelected && 'text-white', // selected
+                    !day.isSelected && day.isCurrentMonth && !day.isToday && 'text-gray-900', // current month and not today and not selected
+                    !day.isSelected && !day.isCurrentMonth && !day.isToday && 'text-gray-400', // not current month and not today and not selected
+                    day.isToday && !day.isSelected && 'text-indigo-600 bg-indigo-100', // today and not selected
+                    dayIdx === 0 && 'rounded-tl-lg', // first day
+                    dayIdx === 6 && 'rounded-tr-lg', // last day
+                    dayIdx === days.length - 7 && 'rounded-bl-lg', // first day of last row
+                    dayIdx === days.length - 1 && 'rounded-br-lg' // last day of last row
+                ]"
+            >
+                <time
+                    :datetime="day.date"
+                    :class="[
+                        'mx-auto flex h-7 w-7 items-center justify-center rounded-full',
+                        day.isSelected && day.isToday && 'bg-indigo-600',
+                        day.isSelected && !day.isToday && 'bg-gray-900'
+                    ]"
+                >
+                    {{ dayjs(day.date).format('D') }}
+                </time>
             </button>
         </div>
     </div>
@@ -14,69 +41,88 @@
 
 <script setup lang="ts">
 import dayjs from 'dayjs'
-import 'dayjs/locale/en'  // load on demand
-import isLeapYear from 'dayjs/plugin/isLeapYear' // import plugin
+import isLeapYear from 'dayjs/plugin/isLeapYear'
 import weekday from 'dayjs/plugin/weekday'
-dayjs.extend(isLeapYear) // use plugin
+import 'dayjs/locale/en'
+import 'dayjs/locale/de'
+
+// Use dayjs plugins
+dayjs.extend(isLeapYear)
 dayjs.extend(weekday)
+
+type CalendarDay = {
+    date: string
+    isCurrentMonth: boolean
+    isToday: boolean
+    isSelected: boolean
+}
+
+type DayName = {
+    short: string
+    long: string
+}
 
 const props = defineProps<{
     timestamp?: number
 }>()
 
-const month = ref('Januar')
-const year = ref('2022')
+// Switch language based on locale
+const { locale } = useI18n()
+switch (locale.value) {
+    case 'de':
+        dayjs.locale('de')
+        break
+    case 'us':
+        dayjs.locale('en')
+        break
+    default:
+        dayjs.locale('de')
+        break
+}
 
-const dayNames = ref([
-    { short: 'M', long: 'Montag' },
-    { short: 'D', long: 'Dienstag' },
-    { short: 'M', long: 'Mittwoch' },
-    { short: 'D', long: 'Donnerstag' },
-    { short: 'F', long: 'Freitag' },
-    { short: 'S', long: 'Samstag' },
-    { short: 'S', long: 'Sonntag' }
-])
+// Get date based on timestamp
+const timestampDate = computed(() => {
+    if (!props.timestamp) return dayjs()
+    return dayjs(props.timestamp * 1000)
+})
 
-const days = computed(() => {
-    // Format date based on timestamp
-    if (!props.timestamp) return false
-    const date = new Date(props.timestamp * 1000)
-    const today = new Date()
+// Get month and year based on timestamp
+const month = computed(() => timestampDate.value.format('MMMM'))
+const year = computed(() => timestampDate.value.format('YYYY'))
 
-    type CalendarDay = {
-        date: string
-        isCurrentMonth: boolean
-        isToday: boolean
-        isSelected: boolean
-    }
+// Get day names for calendar based on locale
+const dayNames = computed<DayName[]>(() => {
+    return Array.from({length: 7}, (_, i) => {
+        const day = dayjs().weekday(i)
+        return {
+            short: day.format('dd'),
+            long: day.format('dddd')
+        }
+    })
+})
 
-    const daysInMonth = dayjs(date).daysInMonth()
-    const firstDay = dayjs(date).startOf('month').weekday()
-    const lastDay = dayjs(date).endOf('month').weekday()
+// Get days for calendar based on timestamp as the current month
+const days = computed<CalendarDay[] | false>(() => {
+    const daysInMonth = timestampDate.value.daysInMonth()
+    const firstDay = timestampDate.value.startOf('month').weekday()
+    const lastDay = timestampDate.value.endOf('month').weekday()
     const days: CalendarDay[] = []
 
-    console.log('Timestamp Date:', date)
-    console.log('Today Date:', today)
-    console.log('Days in Month:', daysInMonth)
-    console.log('First Weekday:', firstDay)
-    console.log('Last Weekday:', lastDay)
-
-    for (let i = 1; i < firstDay; i++) {
-        const prevMonthDate = dayjs(date).subtract(1, 'month').endOf('month').subtract(firstDay - i - 1, 'day')
-        const isToday = dayjs(today).format('YYYY-MM-DD') === prevMonthDate.format('YYYY-MM-DD')
+    for (let i = 0; i < firstDay; i++) {
+        const prevMonthDate = timestampDate.value.subtract(1, 'month').endOf('month').subtract(firstDay - i - 1, 'day')
+        const isToday = dayjs().format('YYYY-MM-DD') === prevMonthDate.format('YYYY-MM-DD')
         days.push({ date: prevMonthDate.format('YYYY-MM-DD'), isCurrentMonth: false, isToday, isSelected: false })
     }
 
     for (let i = 1; i <= daysInMonth; i++) {
-        const isToday = dayjs(today).format('YYYY-MM-DD') === `${dayjs().format('YYYY-MM')}-${String(i).padStart(2, '0')}`
-        const isSelected = dayjs(date).format('YYYY-MM-DD') === `${dayjs().format('YYYY-MM')}-${String(i).padStart(2, '0')}`
-        console.log('isToday', isToday, 'isSelected', isSelected, `${dayjs().format('YYYY-MM')}-${String(i).padStart(2, '0')}`)
-        days.push({ date: `${dayjs(date).format('YYYY-MM')}-${String(i).padStart(2, '0')}`, isCurrentMonth: true, isToday, isSelected })
+        const isToday = dayjs().format('YYYY-MM-DD') === `${dayjs().format('YYYY-MM')}-${String(i).padStart(2, '0')}`
+        const isSelected = timestampDate.value.format('YYYY-MM-DD') === `${dayjs().format('YYYY-MM')}-${String(i).padStart(2, '0')}`
+        days.push({ date: `${timestampDate.value.format('YYYY-MM')}-${String(i).padStart(2, '0')}`, isCurrentMonth: true, isToday, isSelected })
     }
 
-    for (let i = 0; i < 7 - lastDay; i++) {
-        const nextMonthDate = dayjs(date).add(1, 'month').startOf('month').add(i, 'day')
-        const isToday = dayjs(today).format('YYYY-MM-DD') === nextMonthDate.format('YYYY-MM-DD')
+    for (let i = 0; i < 6 - lastDay; i++) {
+        const nextMonthDate = timestampDate.value.add(1, 'month').startOf('month').add(i, 'day')
+        const isToday = dayjs().format('YYYY-MM-DD') === nextMonthDate.format('YYYY-MM-DD')
         days.push({ date: nextMonthDate.format('YYYY-MM-DD'), isCurrentMonth: false, isToday, isSelected: false})
     }
 
