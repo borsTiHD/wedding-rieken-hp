98100fa245a15249137cf82135107a771fc710b7
borsTiHD <bastian.j@gmx.net>
2024-05-08 10:32:58 +0200

    feat: ✨ Asking user for pw reset, if no email was provided

--- src/components/user/ForgotPassword.vue
+++ src/components/user/ForgotPassword.vue
@@ -1,15 +1,21 @@
 <template>
     <ConfirmPopup />
+    <InputPrompt ref="inputPrompt" />
     <Button class="whitespace-nowrap p-0 sm:p-2" :label="t('user.password.resetPassword.forgotButton')" text size="small" :loading="loading" @click="confirmDialog($event)" />
 </template>
 
 <script setup lang="ts">
 import { useToast } from 'primevue/usetoast'
 import { useConfirm } from 'primevue/useconfirm'
+import InputPrompt from '@/components/InputPrompt.vue'
 
-// TODO: Add a Input prompt for the email address if no email is set
+const props = defineProps<{
+    email: string
+}>()
 
 // Refs
+const inputPrompt = ref<InstanceType<typeof InputPrompt>>()
+const email = ref(props.email)
 const loading = ref(false)
 
 // Composables
@@ -18,23 +24,49 @@ const confirm = useConfirm()
 const { t } = useI18n()
 const { sendUserPasswordResetEmail } = useFirebaseAuth()
 
-const props = defineProps<{
-    email: string
-}>()
-
 // Confirm dialog
-const confirmDialog = (event: MouseEvent) => {
-    // Check if email is set
-    if (!props.email || props.email === '') {
-        toast.add({
-            severity: 'error',
-            summary: t('user.password.resetPassword.error'),
-            detail: t('user.password.resetPassword.noMail'),
-            life: 10000
-        })
-        return false
+const confirmDialog = async(event: MouseEvent) => {
+    // Set email from props
+    email.value = props.email
+
+    // Check if email is set and show input prompt if not
+    if (!email.value || email.value === '') {
+        const userInput: { email: string } | false = await inputPrompt.value?.show({
+            title: t('user.password.resetPassword.modalHeader'),
+            message: t('user.password.resetPassword.askForMail'),
+            okButton: t('user.password.resetPassword.submitButton'),
+            cancelButton: t('user.password.resetPassword.cancleButton'),
+            formKitSchema: [{
+                $cmp: 'FormKit',
+                props: {
+                    type: 'email',
+                    name: 'email',
+                    validation: 'required|length:2|email'
+                }
+            }]
+        }).catch((error) => {
+            console.error(error)
+            return false
+        }) as { email: string } || false
+
+        // User canceled
+        if (!userInput) {
+            toast.add({
+                severity: 'error',
+                summary: t('user.password.resetPassword.error'),
+                detail: t('user.password.resetPassword.noMail'),
+                life: 10000
+            })
+            return false
+        }
+
+        // Set email from prompt and submit
+        email.value = userInput.email
+        submit()
+        return true
     }
 
+    // Show confirm dialog and submit the email from props
     confirm.require({
         target: event.currentTarget as HTMLElement,
         message: t('user.password.resetPassword.confirmMessage'),
@@ -42,18 +74,18 @@ const confirmDialog = (event: MouseEvent) => {
         rejectLabel: t('buttons.no'),
         icon: 'pi pi-exclamation-triangle',
         accept: () => {
-            handleSubmit() // Submit Button
+            submit() // Submit Button
         }
     })
 }
 
-// Submit button
-const handleSubmit = async() => {
+// Submit password reset
+const submit = async() => {
     // Start loading
     loading.value = true
 
     // Send password reset email
-    const response = await sendUserPasswordResetEmail(props.email).catch((error: Error) => {
+    const response = await sendUserPasswordResetEmail(email.value).catch((error: Error) => {
         console.error(error)
         toast.add({
             severity: 'error',
