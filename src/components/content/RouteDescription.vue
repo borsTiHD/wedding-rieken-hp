b5ed96aa4378fbb86d0e2e6d00c3cd5e47234fb3
borsTiHD <bastian.j@gmx.net>
2024-05-25 14:10:48 +0200

    feat: ✨ Adding route description content

---
+++ src/components/content/RouteDescription.vue
@@ -0,0 +1,30 @@
+<template>
+    <Card class="card-primary grow">
+        <template #content>
+            <div class="flex flex-col items-center gap-4">
+                <div class="relative inline-block">
+                    <ShowUnderline color="text-rose-400">
+                        <h2 class="font-great-vibes text-4xl md:text-6xl drop-shadow-sm">{{ t('general.address.header') }}</h2>
+                    </ShowUnderline>
+                </div>
+                <dl>
+                    <dt class="text-base font-semibold leading-7 text-gray-900">{{ street }}</dt>
+                    <dd class="text-base leading-7 text-gray-600">{{ city }}</dd>
+                </dl>
+            </div>
+        </template>
+    </Card>
+</template>
+
+<script setup lang="ts">
+import { useAppStore } from '@/stores/app'
+import ShowUnderline from '@/components/animations/ShowUnderline.vue'
+
+// Localisation
+const { t } = useI18n()
+
+// Fetch app config
+const appStore = useAppStore()
+const street = computed(() => appStore.street)
+const city = computed(() => appStore.city)
+</script>
