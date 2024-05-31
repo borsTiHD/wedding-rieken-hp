d94d3a23285144af9de8532d413d924f8aa91aa6
borsTiHD <bastian.j@gmx.net>
2024-05-31 09:10:40 +0200

    feat: 💄 Changing color of the loading spinner

--- src/components/LoadingOverlay.vue
+++ src/components/LoadingOverlay.vue
@@ -2,7 +2,7 @@
     <div class="background-wrapper background-image">
         <div class="background-wrapper flex items-center justify-center bg-opacity-70 bg-body z-50">
             <div class="relative h-16 w-16">
-                <div class="animate-spin rounded-full h-full w-full border-t-4 border-blue-500" />
+                <div class="animate-spin rounded-full h-full w-full border-t-4 border-rose-500" />
                 <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-600">
                     {{ props.progress }}%
                 </div>
