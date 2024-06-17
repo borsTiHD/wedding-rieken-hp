e6ad3311f186135836000ba2debfe932bb78e0ee
borsTiHD <bastian.j@gmx.net>
2024-06-17 11:02:15 +0200

    fix: 🐛 Fixing bug with accessing 'window' on SSR

--- src/composables/useWindowSize.ts
+++ src/composables/useWindowSize.ts
@@ -1,13 +1,26 @@
 import { ref } from 'vue'
 import { useEventListener } from '@/composables/useEventListener'
+
 export function useWindowSize(timer = 500, noTimeout = false) {
+    // Check if window is defined (SSR)
+    if (typeof window === 'undefined' || !window) {
+        return {
+            windowWidth: ref(0),
+            windowHeight: ref(0),
+            scrollX: ref(0),
+            scrollY: ref(0)
+        }
+    }
+
     // On resize window handler - sets the current window size
     const windowWidth = ref(window.innerWidth)
     const windowHeight = ref(window.innerHeight)
     const scrollX = ref(window.scrollX)
     const scrollY = ref(window.scrollY)
+
     let timeoutResize: ReturnType<typeof setTimeout> | null = null
     let timeoutScroll: ReturnType<typeof setTimeout> | null = null
+
     // On resize window handler - sets the current window size
     useEventListener(window, 'resize', () => {
         // If no timeout is set, update the values immediately
@@ -26,6 +39,7 @@ export function useWindowSize(timer = 500, noTimeout = false) {
             }, timer)
         }
     })
+
     // On scroll window handler - sets the current scroll position
     useEventListener(window, 'scroll', () => {
         // If no timeout is set, update the values immediately
@@ -44,5 +58,6 @@ export function useWindowSize(timer = 500, noTimeout = false) {
             }, timer)
         }
     })
+
     return { windowWidth, windowHeight, scrollX, scrollY }
 }
