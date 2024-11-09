c6a049e32beb936254536ea50a6fa6edd8f8f3bf
borsTiHD <bastian.j@gmx.net>
2024-11-09 15:13:12 +0100

    feat: ✨ Adding composable for google maps link

---
+++ app/composables/useGoogleMaps.ts
@@ -0,0 +1,9 @@
+export function useGoogleMaps(address: Ref<string>) {
+    // URL encoded Google maps link for location by address
+    const link = computed(() => {
+        const googleMapsLink = 'https://www.google.com/maps'
+        return address.value ? `${googleMapsLink}/search/?api=1&query=${encodeURIComponent(address.value)}` : googleMapsLink
+    })
+
+    return { link }
+}
