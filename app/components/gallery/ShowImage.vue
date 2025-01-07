f6974bac84b9afed9f0c0bf53cf088e77c4d22b1
borsTiHD <bastian.j@gmx.net>
2025-01-07 20:18:38 +0100

    feat: ✨ enhance image component with hover effects and loading state management

--- app/components/gallery/ShowImage.vue
+++ app/components/gallery/ShowImage.vue
@@ -6,31 +6,39 @@
             originalContainer: 'm-12 p-12'
         }"
         preview
+        @mouseenter="onEnter"
+        @mouseleave="onLeave"
     >
         <template #previewicon>
             <div class="w-full h-full flex items-center justify-center">
                 <span
                     class="text-xl uppercase"
                     :class="[
-                        'group-hover:motion-opacity-in-[0%] group-hover:motion-translate-y-in-[105%] motion-duration-[0.8s]',
+                        {
+                            'motion-opacity-in-[0%] motion-translate-y-in-[105%] motion-duration-[0.8s]': hovered,
+                            // 'motion-opacity-out-[100%] motion-translate-y-out-[0%] motion-duration-[0.8s]': !hovered
+                        },
                         'text-white px-4 py-2 rounded-lg',
-                        // 'motion-opacity-out-[0%] motion-translate-y-out-[105%] motion-duration-[0.8s]'
                     ]"
                 >{{ t('gallery.showImage.preview') }}</span>
             </div>
         </template>
         <template #image>
+            <!-- This is the preview image -->
             <div class="relative" :class="{ 'w-[300px] h-[400px]': loading }">
-                <!-- This is the preview image -->
                 <div v-if="loading" class="absolute inset-0 flex items-center justify-center">
                     <ProgressSpinner class="size-12" />
                 </div>
                 <img
-                    :src="image.itemImageSrc"
+                    :src="image.thumbnailSrc"
                     :alt="image.alt"
                     loading="lazy"
-                    :class="{ 'hide-on-load': loading }"
-                    class="w-full h-full object-cover group-hover:motion-scale-out-[1.4] motion-duration-[10s]"
+                    :class="{
+                        'hide-on-load': loading,
+                        'motion-scale-out-[1.4] motion-duration-[10s]': hovered,
+                        // 'motion-scale-out-[1] motion-duration-[10s]': !hovered
+                    }"
+                    class="w-full h-full object-cover"
                     @load="onLoad"
                     @error="onError"
                 >
@@ -39,7 +47,7 @@
         <template #original="slotProps">
             <!-- This is the original image (fullscreen) -->
             <img
-                :src="image.itemImageSrc"
+                :src="image.src"
                 :alt="image.alt"
                 loading="lazy"
                 :style="slotProps.style"
@@ -50,6 +58,7 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import type { Image } from '@/types/Image'
 
 type Props = { image: Image }
@@ -58,16 +67,15 @@ const { image } = toRefs(props)
 
 const { t } = useI18n()
 
-import { ref } from 'vue'
+// Image loading and error handling
 const loading = ref(true)
+const onLoad = () => { loading.value = false }
+const onError = () => { loading.value = false }
 
-const onLoad = () => {
-    loading.value = false
-}
-
-const onError = () => {
-    loading.value = false
-}
+// Image hover effect
+const hovered = ref(false)
+const onEnter = () => { hovered.value = true }
+const onLeave = () => { hovered.value = false }
 </script>
 
 <style scoped>
