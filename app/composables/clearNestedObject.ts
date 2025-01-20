1db7d72a3db345b79119fa812cc14ab680a79ff0
borsTiHD <bastian.j@gmx.net>
2025-01-20 19:43:10 +0100

    fix: comment out recursive call in clearNestedObject function

--- app/composables/clearNestedObject.ts
+++ app/composables/clearNestedObject.ts
@@ -1,10 +1,10 @@
 export default function clearNestedObject(obj: Record<string, unknown>) {
   for (const prop in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, prop)) {
-      if (typeof obj[prop] === 'object' && obj[prop] !== null && !Array.isArray(obj[prop])) {
-        // If the property is an object, call the function recursively
-        clearNestedObject(obj[prop] as Record<string, unknown>)
-      }
+      // if (typeof obj[prop] === 'object' && obj[prop] !== null && !Array.isArray(obj[prop])) {
+      //   // If the property is an object, call the function recursively
+      //   clearNestedObject(obj[prop] as Record<string, unknown>)
+      // }
       // Delete the property
       delete obj[prop]
     }
