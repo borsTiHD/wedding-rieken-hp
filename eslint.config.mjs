1d6defa9b77e117db931a7b2c6f6a14350e7135d
borsTiHD <bastian.j@gmx.net>
2025-01-21 19:19:42 +0100

    chore: 🛠️ update ESLint configuration and add TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,19 @@
 import antfu from '@antfu/eslint-config'
+import vue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
 
-export default antfu({
-  vue: true,
-})
+export default antfu(
+  {
+    // Configures for antfu's config
+    vue: true,
+    typescript: true,
+  },
+  // From the second arguments they are ESLint Flat Configs
+  // you can have multiple configs
+  {
+    plugins: {
+      vue,
+      tseslint,
+    },
+  },
+)
