6410e670ad3d5b7c92f96680513f250532ff3c83
borsTiHD <bastian.j@gmx.net>
2025-03-11 22:40:14 +0100

    feat: ✨ enhance ShowGallery component layout with additional image display sections and improved spacing

--- app/components/gallery/ShowGallery.vue
+++ app/components/gallery/ShowGallery.vue
@@ -33,13 +33,52 @@ const { t } = useI18n()
     <Card>
       <template #content>
         <IconBackground icon="pi-image" />
-        <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center gap-4">
           <div class="flex flex-col items-center">
             <div v-if="images && images.length" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
               <ShowImage v-for="(picture, index) in images" :key="index" :image="picture" />
             </div>
             <p v-else>
               {{ t('gallery.noImages') }}
+            </p>
+          </div>
+        </div>
+
+        <hr class="my-8 border-t border-gray-300 w-full">
+
+        <!-- <div class="flex flex-col items-center gap-4">
+          <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div v-for="(image, index) in images" :key="index" class="p-4">
+              <ShowImage :image="image" />
+            </div>
+          </div>
+        </div>
+
+        <hr class="my-8 border-t border-gray-300 w-full"> -->
+
+        <div class="flex flex-col items-center gap-4">
+          <div class="flex flex-wrap">
+            <div class="flex w-1/2 flex-wrap">
+              <div v-for="(image, index) in images.slice(0, 3)" :key="index" class="flex p-4" :class="index === 2 ? 'w-full' : 'w-1/2'">
+                <ShowImage :key="index" :image="image" />
+              </div>
+            </div>
+            <div class="flex w-1/2 flex-wrap">
+              <div v-for="(image, index) in images.slice(3, 6)" :key="index" class="flex p-4" :class="index === 0 ? 'w-full' : 'w-1/2'">
+                <ShowImage :key="index" :image="image" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+    </Card>
+    <Card>
+      <template #content>
+        <IconBackground icon="pi-image" />
+        <div class="flex flex-col items-center gap-4">
+          <div class="flex flex-col items-center">
+            <p class="text-center text-md md:text-lg text-balance">
+              {{ t('gallery.footerText') }}
             </p>
           </div>
         </div>
