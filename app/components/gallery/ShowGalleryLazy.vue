e1d4f242ad840739a1a075d6b66385d37d461959
borsTiHD <bastian.j@gmx.net>
2025-04-21 11:27:15 +0200

    feat: implement lazy loading for gallery images with improved sorting and filtering - created for the new backend file server

---
+++ app/components/gallery/ShowGalleryLazy.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import ShowUnderline from '@/components/animations/ShowUnderline.vue'
+import ShowImageLazy from '@/components/gallery/ShowImageLazy.vue'
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
+
+interface Props { imagePaths: string[] }
+const props = defineProps<Props>()
+const { imagePaths } = toRefs(props)
+
+const onlyFirstTen = computed(() => {
+  // if (imagePaths.value.length > 10) {
+  //   return imagePaths.value.slice(0, 10)
+  // }
+  return imagePaths.value
+})
+
+const { t } = useI18n()
+
+// Window size for mobile check
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const smAndLarger = breakpoints.greaterOrEqual('sm') // sm and larger
+
+function checkIndexSmallDevice(index: number): boolean {
+  return Math.floor(index / 2) % 2 === 1
+}
+
+function checkIndexBigDevice(index: number): boolean {
+  const cycleLength = 6 // 2 false -> 2 true -> 4 false -> 2 true -> 4 false...
+  const positionInCycle = index % cycleLength
+  return positionInCycle >= 2 && positionInCycle < 4
+}
+
+function checkIndex(index: number): boolean {
+  if (smAndLarger.value) {
+    return checkIndexBigDevice(index)
+  }
+  else {
+    return checkIndexSmallDevice(index)
+  }
+}
+
+function imageClasses(index: number) {
+  return [
+    { 'col-span-2 row-span-2': checkIndex(index) },
+    { 'col-span-1 row-span-1': !checkIndex(index) },
+  ]
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <Card>
+      <template #content>
+        <IconBackground icon="pi-image" />
+        <div class="flex flex-col items-center gap-4">
+          <!-- <ShowUnderline color="text-blue-400"> -->
+          <ShowUnderline color="text-[#f2b69a]">
+            <h2 class="font-great-vibes text-4xl md:text-6xl drop-shadow-sm">
+              {{ t('gallery.header') }}
+            </h2>
+          </ShowUnderline>
+          <div class="flex flex-col items-center gap-4">
+            <p class="text-center text-md md:text-lg text-balance">
+              {{ t('gallery.description') }}
+            </p>
+          </div>
+        </div>
+      </template>
+    </Card>
+    <Card>
+      <template #content>
+        <IconBackground icon="pi-image" />
+        <div class="flex flex-col items-center">
+          <div v-if="imagePaths && imagePaths.length" class="grid md:grid-cols-4 gap-4">
+            <div
+              v-for="(path, index) in onlyFirstTen"
+              :key="`${index}-${path}`"
+              class="flex"
+              :class="imageClasses(index)"
+            >
+              <ShowImageLazy :image-path="path" />
+            </div>
+          </div>
+          <p v-else>
+            {{ t('gallery.noImages') }}
+          </p>
+        </div>
+      </template>
+    </Card>
+  </div>
+</template>
--- app/pages/gallery.vue
+++ app/pages/gallery.vue
@@ -1,40 +1,31 @@
 <script setup lang="ts">
-import type { Image } from '@/types/Image'
-import ShowGallery from '@/components/gallery/ShowGallery.vue'
-import { useFilesQueries } from '@/queries/useFilesQueries'
+import ShowGalleryLazy from '@/components/gallery/ShowGalleryLazy.vue'
 import { useFolderQuery } from '@/queries/useFolderQuery'
 
 const galleryPath = ref('gallery/')
 const { data: filesData, isLoading: isLoadingFolder, isFetching: isFetchingFolder } = useFolderQuery(galleryPath)
 const loadingFolder = computed(() => isLoadingFolder.value || isFetchingFolder.value)
-const imagePaths = computed(() => {
-  return filesData?.value?.files?.map((item) => {
-    // Type guard to ensure 'item.file' has a 'name' property
-    if ('name' in item?.file) {
-      return item.file.name // Complete path for downloading preview
-    }
-    return null // Handle cases where 'name' doesn't exist
-  }).filter(file => file !== null) as string[] // Remove null entries and assert type
-})
 
-const imageDataQueries = useFilesQueries(imagePaths, ref(true))
-// const loadingImages = computed(() => imageDataQueries.value.isLoading)
-const images = computed<Image[]>(() => {
-  return imageDataQueries.value.data?.map((item) => {
-    if (!item?.previewUrl) {
-      return null // Handle cases where 'previewUrl' doesn't exist
-    }
-    return {
-      src: item?.previewUrl,
-      thumbnailSrc: item?.previewUrl,
-      alt: item?.file?.path || '',
-      title: item?.file?.name,
-      lastModified: item?.file?.metadata?.lastModified,
-      size: item?.file?.metadata?.size,
-    }
-  })?.filter(file => file !== null)?.sort((a, b) => {
-    return new Date(b.lastModified).getTime() - new Date(a.lastModified).getTime()
-  })
+// Get only image paths
+// Sort by last modified date
+const imagePaths = computed(() => {
+  return filesData?.value?.files
+    ?.slice()
+    ?.sort((a, b) => {
+      // Type guard to ensure 'item.file' has a 'name' property
+      if ('lastModified' in a?.file && 'lastModified' in b?.file) {
+        return new Date(b?.file?.lastModified).getTime() - new Date(a?.file?.lastModified).getTime()
+      }
+      return 0
+    })
+    ?.map((item) => {
+      // Type guard to ensure 'item.file' has a 'name' property
+      if ('name' in item?.file) {
+        return item.file.name // Complete path for downloading preview
+      }
+      return null // Handle cases where 'name' doesn't exist
+    })
+    ?.filter(file => file !== null) as string[] // Remove null entries and assert type
 })
 </script>
 
@@ -44,7 +35,7 @@ const images = computed<Image[]>(() => {
       <div v-if="loadingFolder" class="flex items-center justify-center">
         <i class="pi pi-spin pi-spinner text-gray-500 text-2xl" />
       </div>
-      <ShowGallery v-else :images="images" />
+      <ShowGalleryLazy v-else :image-paths="imagePaths" />
     </div>
   </main>
 </template>
