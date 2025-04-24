1861fde17ef6835a5bba86f1cb1e7bc32c0e0d9b
borsTiHD <bastian.j@gmx.net>
2025-04-24 23:08:59 +0200

    fix: simplify welcome message by removing deadline reference and unused config

--- app/pages/welcome.vue
+++ app/pages/welcome.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
 import ShowUnderline from '@/components/animations/ShowUnderline.vue'
-import { useConfig } from '@/composables/useConfig'
 import { useTokenStore } from '@/stores/token'
 import { useUserStore } from '@/stores/user'
 
 // Composables
 const router = useRouter()
-const { t, d } = useI18n()
+const { t } = useI18n()
 const localePath = useLocalePath()
-
-// Config
-const { weddingDate, deadlineDate, isBeforeDeadline } = useConfig()
 
 // User store
 const userStore = useUserStore()
@@ -62,14 +58,8 @@ onMounted(() => {
         </h3>
 
         <!-- Welcome message -->
-        <div v-if="isBeforeDeadline" class="flex flex-col items-center gap-4">
+        <div class="flex flex-col items-center gap-4">
           <i18n-t keypath="welcome.welcomeMessage" tag="p">
-            <template #weddingDate>
-              <span class="font-bold whitespace-nowrap">{{ d(weddingDate || 0, 'short') }}</span>
-            </template>
-            <template #deadline>
-              <span class="font-bold whitespace-nowrap">{{ d(deadlineDate || 0, 'short') }}</span>
-            </template>
             <template #action>
               <NuxtLink :to="localePath('/register')" class="font-medium text-blue-600">
                 {{ t('welcome.loginLinkTag') }}
@@ -85,15 +75,6 @@ onMounted(() => {
               <Button class="grow" :label="t('register.submit')" icon="pi pi-user-plus" fluid />
             </NuxtLink>
           </div>
-        </div>
-
-        <!-- After deadline message -->
-        <div v-else class="flex flex-col items-center gap-4">
-          <i18n-t keypath="welcome.welcomeMessageAfterDeadline" tag="p">
-            <template #weddingDate>
-              <span class="font-bold whitespace-nowrap">{{ d(weddingDate || 0, 'short') }}</span>
-            </template>
-          </i18n-t>
         </div>
       </template>
     </Card>
