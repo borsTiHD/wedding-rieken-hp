e8421ecbf2790a62f17688f32f247a179e4c684f
borsTiHD <bastian.j@gmx.net>
2025-06-22 16:57:21 +0200

    feat: extend ImageFile interface to include metadata and file properties

--- shared/types/ImageFile.ts
+++ shared/types/ImageFile.ts
@@ -1,6 +1,24 @@
+export interface FtpFile {
+  name: string
+  size: number
+  type: string
+  modifyTime: number
+  accessTime: number
+  rights: {
+    user: string
+    group: string
+    other: string
+  }
+  owner: number
+  group: number
+  longname: string
+}
+
 export interface ImageFile {
-  metadata: any
-  file: any
+  name: string
+  lastModified: Date
+  size: number
+  type: string
 }
 
 export interface ItemMetadata {
