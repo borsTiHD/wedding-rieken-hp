88c572e2b630609d199b573852c213bf2ab76296
borsTiHD <bastian.j@gmx.net>
2025-06-22 10:07:11 +0200

    feat: implement file upload functionality in gallery user page and remove proof of concept in the other gallery

--- app/components/admin/ListGalleryFiles.vue
+++ app/components/admin/ListGalleryFiles.vue
@@ -81,35 +81,6 @@ async function downloadAll() {
     console.error('Error downloading folder:', error)
   })
 }
-
-const fileInput = ref<HTMLInputElement | null>(null)
-
-function triggerFileInput() {
-  fileInput.value?.click()
-}
-
-function onFileChange(event: Event) {
-  const target = event.target as HTMLInputElement
-  const file = target.files && target.files[0]
-  // console.log('event:', event)
-  if (file) {
-    // hier kannst du das Bild an dein Backend schicken
-    uploadPhoto(file as File)
-  }
-}
-
-async function uploadPhoto(file: File) {
-  const formData = new FormData()
-  formData.append('photo', file)
-
-  // console.log('file:', file)
-  // console.log('Uploading photo:', file.name)
-  // console.log('Form data:', formData)
-  // await fetch('/api/upload', {
-  //   method: 'POST',
-  //   body: formData,
-  // });
-}
 </script>
 
 <template>
@@ -121,18 +92,6 @@ async function uploadPhoto(file: File) {
     </template>
     <template #content>
       <div class="flex flex-col gap-4">
-        <div>
-          <input
-            ref="fileInput"
-            type="file"
-            accept="image/*"
-            capture="environment"
-            style="display: none"
-            @change="onFileChange"
-          >
-          <Button label="Foto aufnehmen" @click="triggerFileInput" />
-        </div>
-
         <DataTable
           v-model:selection="selectedFiles"
           :value="files"
--- app/pages/gallery_user.vue
+++ app/pages/gallery_user.vue
@@ -1,12 +1,51 @@
 <script setup lang="ts">
 const { refreshToken } = useFirebaseAuth()
 refreshToken() // Refresh token to avoid 401 error
+
+const fileInput = ref<HTMLInputElement | null>(null)
+
+function triggerFileInput() {
+  fileInput.value?.click()
+}
+
+function onFileChange(event: Event) {
+  const target = event.target as HTMLInputElement
+  const file = target.files && target.files[0]
+  // console.log('event:', event)
+  if (file) {
+    // hier kannst du das Bild an dein Backend schicken
+    uploadPhoto(file as File)
+  }
+}
+
+async function uploadPhoto(file: File) {
+  const formData = new FormData()
+  formData.append('photo', file)
+
+  // console.log('file:', file)
+  // console.log('Uploading photo:', file.name)
+  // console.log('Form data:', formData)
+  // await fetch('/api/upload', {
+  //   method: 'POST',
+  //   body: formData,
+  // });
+}
 </script>
 
 <template>
   <main class="mx-auto flex flex-col">
     <div class="p-4 mx-auto sm:w-11/12 md:w-10/12 lg:w-8/12 flex flex-col gap-4">
-      xxx
+      <div>
+        <input
+          ref="fileInput"
+          type="file"
+          accept="image/*"
+          capture="environment"
+          style="display: none"
+          @change="onFileChange"
+        >
+        <Button label="Foto aufnehmen" @click="triggerFileInput" />
+      </div>
     </div>
   </main>
 </template>
