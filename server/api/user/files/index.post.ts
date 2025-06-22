74765c5fcc9acba216455274e0998807920fa9c0
borsTiHD <bastian.j@gmx.net>
2025-06-22 13:48:27 +0200

    feat: refactor file upload logic to streamline path handling and improve error messaging

--- server/api/user/files/index.post.ts
+++ server/api/user/files/index.post.ts
@@ -16,12 +16,8 @@ export default defineEventHandler(async (event) => {
     throw createError({ statusCode: 400, statusMessage: 'No files uploaded' })
   }
 
-  // Get file path from form data
-  // Also set the user ID in the path
-  const pathRaw = formData.find(field => field?.name === 'path')
-  const pathRawString = pathRaw?.data ? `/${pathRaw?.data?.toString()}` : ''
-  const pathUser = `/user/${user.uid}`
-  const path = `${pathUser}${pathRawString}`
+  // Set path for user files
+  const userPath = `/user/${user.uid}`
 
   // Get file from form data
   const file = formData.find(field => field?.name === 'file' && field?.type && field?.data && field?.data?.length > 0 && field?.filename)
@@ -32,7 +28,6 @@ export default defineEventHandler(async (event) => {
 
   // Get file information
   const fileName = file.filename || `${randomUUID()}` // Original file name
-  const fileFullPath = `${path}/${fileName}`
   const mimeType = file.type // MIME-Type (e.g., image/png)
   const fileBuffer = file.data // Buffer of the file
 
@@ -43,9 +38,9 @@ export default defineEventHandler(async (event) => {
   }
 
   // Upload file
-  await uploadFile(fileName, fileFullPath, mimeType, fileBuffer).catch((error) => {
+  await uploadFile(fileName, userPath, mimeType, fileBuffer).catch((error) => {
     console.error('File upload failed:', error)
-    throw createError({ statusCode: 500, statusMessage: 'Failed to upload file' })
+    throw createError({ statusCode: 500, statusMessage: error.message || 'Failed to upload file' })
   })
 
   // Return
--- server/lib/filesApi.ts
+++ server/lib/filesApi.ts
@@ -1,20 +1,34 @@
 import type { ImageFile, ItemMetadata } from '@@/shared/types/ImageFile'
 import type { Buffer } from 'node:buffer'
+import { generateMediumImage, generateThumbnail, getMediumPath, getThumbnailPath } from '@@/server/lib/filesHelper'
 import SftpClass from '@@/server/lib/sftp'
-import sharp from 'sharp'
 
-async function uploadFile(fileName: string, filePath: string, fileType: string, fileBuffer: Buffer<ArrayBufferLike>) {
+async function uploadFile(fileName: string, userPath: string, fileType: string, fileBuffer: Buffer<ArrayBufferLike>) {
+  // Upload the original file
+  const origFilePath = `${userPath}/${fileName}`
+  await uploadFileHelper(origFilePath, fileBuffer)
+
+  // Generate and upload thumbnail
+  const thumbnailBuffer = await generateThumbnail(fileBuffer)
+  const thumbnailPath = getThumbnailPath(userPath, fileName) // Ensure thumbnail has .jpg extension
+  await uploadFileHelper(thumbnailPath, thumbnailBuffer)
+
+  // Generate and upload medium image
+  const mediumBuffer = await generateMediumImage(fileBuffer)
+  const mediumPath = getMediumPath(userPath, fileName) // Ensure medium image has .jpg extension
+  await uploadFileHelper(mediumPath, mediumBuffer)
+
+  return true
+}
+
+async function uploadFileHelper(filePath: string, fileBuffer: Buffer<ArrayBufferLike>) {
   // Initialize SFTP client
   const SftpClient = new SftpClass()
-
   await SftpClient.connect()
+
   try {
-    console.log(`Uploading file: ${fileName} to ${filePath}`)
-    console.log(`fileName:`, fileName)
-    console.log(`filePath:`, filePath)
     // Upload the file to the SFTP server
     await SftpClient.upload(filePath, fileBuffer)
-    console.log(`File uploaded successfully: ${fileName} to ${filePath}`)
   }
   catch (error: any) {
     console.error(`Failed to upload file: ${error.message}`)
@@ -24,80 +38,15 @@ async function uploadFile(fileName: string, filePath: string, fileType: string, 
     // Close the SFTP connection
     await SftpClient.end()
   }
-
-  return true
-
-  // Check if bucket exists
-  // const bucketExists = await checkBucketExists(bucket)
-
-  // if (!bucketExists) {
-  //   throw new Error('Minio: Bucket does not exist')
-  // }
-
-  // const metaData: ItemMetadata = {
-  //   'Content-Type': fileType,
-  //   'Content-Length': fileBuffer.length.toString(),
-  //   'X-Amz-Meta-Original-Filename': fileName,
-  //   'X-Amz-Meta-Original-Path': filePath,
-  //   'X-Amz-Meta-Original-Filetype': fileType,
-  // }
-
-  // // Generate a thumbnail
-  // const thumbnailBuffer = await sharp(fileBuffer)
-  //   .rotate() // Normalize orientation based on EXIF metadata
-  //   .resize({ width: 400, withoutEnlargement: true }) // Resize to 400px width while maintaining aspect ratio
-  //   .jpeg({ quality: 60 }) // Convert to JPEG with 60% quality
-  //   .toBuffer()
-
-  // // Generate a medium image
-  // const mediumBuffer = await sharp(fileBuffer)
-  //   .rotate() // Normalize orientation based on EXIF metadata
-  //   .resize({ width: 1000, withoutEnlargement: true }) // Resize to 1000px width while maintaining aspect ratio
-  //   .jpeg({ quality: 60 }) // Convert to JPEG with 60% quality
-  //   .toBuffer()
-
-  // // Upload the thumbnail
-  // const thumbnailPath = getThumbnailPath(filePath) // Ensure thumbnail has .jpg extension
-  // await MinioClient.putObject(
-  //   bucket,
-  //   thumbnailPath,
-  //   thumbnailBuffer,
-  //   thumbnailBuffer.length, // Optional size
-  //   {
-  //     ...metaData,
-  //     'X-Amz-Meta-Thumbnail': 'true',
-  //   } as ItemBucketMetadata,
-  // )
-
-  // // Upload the medium image
-  // const mediumPath = getMediumPath(filePath) // Ensure medium image has .jpg extension
-  // await MinioClient.putObject(
-  //   bucket,
-  //   mediumPath,
-  //   mediumBuffer,
-  //   mediumBuffer.length, // Optional size
-  //   {
-  //     ...metaData,
-  //     'X-Amz-Meta-Medium': 'true',
-  //   } as ItemBucketMetadata,
-  // )
-
-  // return MinioClient.putObject(
-  //   bucket,
-  //   filePath,
-  //   fileBuffer,
-  //   fileBuffer.length, // Optional size
-  //   metaData,
-  // )
 }
 
 export {
-//   deleteFile,
-//   downloadFile,
-//   downloadFolder,
-//   getAllFilesPaginated,
-//   getListAllFiles,
-//   getMetadata,
-//   getPreviewUrl,
+  //   deleteFile,
+  //   downloadFile,
+  //   downloadFolder,
+  //   getAllFilesPaginated,
+  //   getListAllFiles,
+  //   getMetadata,
+  //   getPreviewUrl,
   uploadFile,
 }
